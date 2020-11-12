79b5b13adae4ba77797f603d8a576df30f19b1c3
Raul Melo <melo.raulf@gmail.com>
2020-11-12 21:59:23 +0100

    Merge pull request #18 from raulfdm/issue-4-hide-theme-switcher-in-blo
    feat: hide switch locale when in a blog post

--- apps/raulmelo-dev-web/src/components/MenuBar/components/LanguageSwitch/LanguageSwitch.tsx
+++ apps/raulmelo-dev-web/src/components/MenuBar/components/LanguageSwitch/LanguageSwitch.tsx
@@ -1,5 +1,6 @@
 import { Language } from '@styled-icons/material/Language';
 import { FormattedMessage } from 'react-intl';
+import { useRouter } from 'next/router';
 
 import { useLocalization } from '@hooks/useLocalization';
 import { DropdownMenu, DropdownMenuItem } from '@components/DropdownMenu';
@@ -7,6 +8,22 @@ import { MenuButton } from '@components/MenuBar';
 
 export const LanguageSwitch: React.FC = () => {
   const { switchToEnglish, switchToPortuguese } = useLocalization();
+  const { pathname } = useRouter();
+
+  /**
+   * Having this option in a post Page leads into an undesired behaviour.
+   *
+   * Not all posts has translations. It means that if the user try to switch
+   * locale in a post which does not its equivalent in the selected language,
+   * it'll throw redirects the user to a 404 page.
+   *
+   * For handling posts translations I have the "AvailableTranslations" component
+   * which allow the user know when this post has translations available and also
+   * redirects he/she for the correct page
+   */
+  if (pathname.includes('/blog/')) {
+    return null;
+  }
 
   return (
     <DropdownMenu
