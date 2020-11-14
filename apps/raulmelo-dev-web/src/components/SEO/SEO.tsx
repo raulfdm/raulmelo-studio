7c426e22ed73bfbe34c700f55c44a45d1997c93e
Raul Melo <melo.raulf@gmail.com>
2020-11-14 13:51:31 +0100

    Merge pull request #30 from raulfdm/add-fav-icon
    feat: add fav icon and web manifest

--- apps/raulmelo-dev-web/src/components/SEO/SEO.tsx
+++ apps/raulmelo-dev-web/src/components/SEO/SEO.tsx
@@ -52,6 +52,28 @@ export const SEO: React.FC<SEOProps> = (props) => {
       <meta name="twitter:image" content={imageUrl} />
       <meta name="twitter:image" content={imageUrl} />
 
+      {/* Fav Icon */}
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
+      <meta name="theme-color" content="#ffffff" />
+
       {/* Monetize my content */}
       {/* TODO: disable this by default and only enable via props */}
       <meta name="monetization" content="$ilp.uphold.com/Aa8j4MXjnPHg" />
