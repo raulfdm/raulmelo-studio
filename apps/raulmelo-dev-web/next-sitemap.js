e167168908597b9c62808ca02e6a871d255517df
Raul Melo <melo.raulf@gmail.com>
2020-11-15 17:08:24 +0100

    Merge pull request #41 from raulfdm/add-sitemaps-xml
    Add sitemaps xml

---
+++ apps/raulmelo-dev-web/next-sitemap.js
@@ -0,0 +1,16 @@
+// https://github.com/iamvishnusankar/next-sitemap
+module.exports = {
+  siteUrl: 'https://raulmelo.dev',
+  generateRobotsTxt: true,
+  exclude: ['/404'],
+  sitemapSize: 7000,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/search', '/404'],
+      },
+    ],
+  },
+};
