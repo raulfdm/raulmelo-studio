282cd9830105c1f820aaedd6ba83881001e0f475
Raul Melo <melo.raulf@gmail.com>
2020-11-15 18:43:59 +0100

    Merge pull request #44 from raulfdm/fix-animation
    fix: animations and refactor component

--- apps/raulmelo-dev-web/src/screens/Blog/components/SeriesSection/SeriesSection.tsx
+++ apps/raulmelo-dev-web/src/screens/Blog/components/SeriesSection/SeriesSection.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import { FormattedMessage } from 'react-intl';
 import Link from 'next/link';
 
 import { Container } from '@components/Ui';
 import { ArrowIosDownwardOutline } from '@styled-icons/evaicons-outline/ArrowIosDownwardOutline';
-import * as S from './styled';
+import {
+  Card,
+  ExpanderButton,
+  Info,
+  Item,
+  List,
+  MenuFooter,
+  Wrapper,
+} from './styled';
 import { RelevantPostSerieData } from '@screens/Blog/utils/series';
 
 interface SeriesSectionProps {
@@ -13,94 +21,150 @@ interface SeriesSectionProps {
   divider?: boolean;
 }
 
+const variants = {
+  list: {
+    open: {
+      height: 'auto',
+      opacity: 1,
+      transition: {
+        delayChildren: 0.2,
+        restDelta: 2,
+        staggerChildren: 0.07,
+        stiffness: 40,
+        type: 'spring',
+      },
+    },
+    collapsed: {
+      opacity: 0,
+      height: 0,
+      transition: {
+        damping: 40,
+        delay: 0.6,
+        staggerChildren: 0.05,
+        staggerDirection: -1,
+        stiffness: 400,
+        type: 'spring',
+      },
+    },
+  },
+  item: {
+    open: {
+      y: 0,
+      height: 'auto',
+      opacity: 1,
+      transition: {
+        stiffness: 1000,
+        velocity: -200,
+      },
+    },
+    collapsed: {
+      y: 50,
+      height: 0,
+      opacity: 0,
+      transition: {
+        stiffness: 1000,
+      },
+    },
+  },
+};
+
 export const SeriesSection: React.FC<SeriesSectionProps> = ({
   series,
   currentPostId,
 }) => {
-  return (
-    <Container data-testid="series-section" as="section">
-      <SeriesMenu series={series} currentPostId={currentPostId} />
-    </Container>
-  );
-};
-
-type SeriesMenuProps = Pick<SeriesSectionProps, 'series' | 'currentPostId'>;
-
-function SeriesMenu({ series, currentPostId }: SeriesMenuProps) {
-  const [isOpen, setIsOpen] = React.useState(false);
+  const [isOpen, setIsOpen] = useState(false);
   const { name, posts, amount } = series;
 
-  React.useEffect(() => {
+  useEffect(() => {
     if (isOpen) {
       setIsOpen(false);
     }
   }, [currentPostId]);
 
+  const toggleSection = () => setIsOpen(!isOpen);
+
   return (
-    <S.Card data-testid="series-menu">
-      <S.Wrapper>
-        <S.Info
-          expanded={isOpen}
-          onClick={() => setIsOpen(!isOpen)}
-          data-testid="expand-button"
-        >
-          <span>{name}</span>
-          <S.ExpanderButton
-            initial="collapsed"
+    <Container data-testid="series-section" as="section">
+      <Card data-testid="series-menu">
+        <Wrapper>
+          <Header isOpen={isOpen} toggleSection={toggleSection} name={name} />
+          <List
+            initial={false}
             animate={isOpen ? 'open' : 'collapsed'}
-            variants={{
-              open: { rotate: '0deg' },
-              collapsed: { rotate: '180deg' },
-            }}
+            variants={variants.list}
+            data-testid="series-post-list"
           >
-            <ArrowIosDownwardOutline size={21} />
-          </S.ExpanderButton>
-        </S.Info>
-        <S.List
-          animate={isOpen ? 'open' : 'collapsed'}
-          variants={{
-            open: { opacity: 1, height: 'auto' },
-            collapsed: { opacity: 0, height: 0 },
-          }}
-          transition={{ ease: [0.4, 0, 0.2, 1] }}
-          data-testid="series-post-list"
-        >
-          {posts!.map((post) => {
-            const { id, copy, uri } = post!;
-            /* TODO: fix this animation. It's wacky. */
-            return (
-              <S.Item
-                key={id}
-                animate={isOpen ? 'open' : 'collapsed'}
-                className={id === currentPostId ? 'active' : ''}
-                data-testid={`post_${id}`}
-                variants={{
-                  open: { opacity: 1, height: 'auto' },
-                  collapsed: { opacity: 0, height: 0 },
-                }}
-              >
-                {/*
+            {posts!.map((post) => {
+              const { id, copy, uri } = post!;
+              /* TODO: fix this animation. It's wacky. */
+              return (
+                <Item
+                  key={id}
+                  className={id === currentPostId ? 'active' : ''}
+                  data-testid={`post_${id}`}
+                  variants={variants.item}
+                >
+                  {/*
                   To proper work it needs next 10.0.2
                   https://github.com/vercel/next.js/issues/19007 
                 */}
-                <Link href={uri}>
-                  <a>{copy}</a>
-                </Link>
-              </S.Item>
-            );
-          })}
-        </S.List>
-        <S.MenuFooter expanded={isOpen} onClick={() => setIsOpen(!isOpen)}>
-          <span>
-            <FormattedMessage
-              id="series.sectionDescription"
-              values={{
-                seriesAmount: amount,
-              }}
-            />
-          </span>
-        </S.MenuFooter>
-      </S.Wrapper>
-    </S.Card>
+                  <Link href={uri}>
+                    <a>{copy}</a>
+                  </Link>
+                </Item>
+              );
+            })}
+          </List>
+          <Footer
+            isOpen={isOpen}
+            toggleSection={toggleSection}
+            amount={amount}
+          />
+        </Wrapper>
+      </Card>
+    </Container>
   );
-}
+};
+
+type HeaderProps = {
+  toggleSection: () => void;
+  isOpen: boolean;
+  name: string;
+};
+
+const Header = ({ isOpen, toggleSection, name }: HeaderProps) => {
+  return (
+    <Info expanded={isOpen} onClick={toggleSection} data-testid="expand-button">
+      <span>{name}</span>
+      <ExpanderButton
+        initial="collapsed"
+        animate={isOpen ? 'open' : 'collapsed'}
+        variants={{
+          open: { rotate: '0deg' },
+          collapsed: { rotate: '180deg' },
+        }}
+      >
+        <ArrowIosDownwardOutline size={21} />
+      </ExpanderButton>
+    </Info>
+  );
+};
+
+type FooterProps = Pick<HeaderProps, 'isOpen' | 'toggleSection'> & {
+  amount: number;
+};
+
+const Footer = ({ isOpen, amount, toggleSection }: FooterProps) => {
+  return (
+    <MenuFooter expanded={isOpen} onClick={toggleSection}>
+      <span>
+        <FormattedMessage
+          id="series.sectionDescription"
+          values={{
+            seriesAmount: amount,
+          }}
+        />
+      </span>
+    </MenuFooter>
+  );
+};
