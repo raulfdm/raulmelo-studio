65396cb775888deba2addeda030fdd892b0fc9cf
Raul Melo <melo.raulf@gmail.com>
2020-12-27 21:37:28 +0100

    ci: fix semantic-release branch name

--- packages/raulmelo-dev-components/release.config.js
+++ packages/raulmelo-dev-components/release.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  release: {
-    branches: ['main', { name: 'setup-semantic-release', prerelease: true }],
-  },
+  branches: ['main'],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
