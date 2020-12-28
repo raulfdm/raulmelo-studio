549b2c1e712d2ca93686b5fa32f27a5f19e0f3e0
Raul Melo <melo.raulf@gmail.com>
2020-12-28 22:11:11 +0100

    chore: remove dummy button component

--- packages/raulmelo-dev-components/src/button/Button.tsx
+++
@@ -1,8 +0,0 @@
-import React, { ButtonHTMLAttributes } from 'react';
-import classNames from 'classnames';
-
-export const Button = ({ className, ...props }: ButtonProps) => (
-  <button className={classNames(['bg-purple-600', className])} {...props} />
-);
-
-export type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
