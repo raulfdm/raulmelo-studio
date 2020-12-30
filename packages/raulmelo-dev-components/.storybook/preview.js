5ac76c5d6a8fc38ca00da742b8df430b75517c20
Raul Melo <melo.raulf@gmail.com>
2020-12-30 14:42:15 +0100

    docs: add fonts, body class, and theme

--- packages/raulmelo-dev-components/.storybook/preview.js
+++ packages/raulmelo-dev-components/.storybook/preview.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import '../static/css/base.css';
 
 // https://storybook.js.org/docs/react/writing-stories/parameters#global-parameters
@@ -5,3 +6,38 @@ export const parameters = {
   // https://storybook.js.org/docs/react/essentials/actions#automatically-matching-args
   actions: { argTypesRegex: '^on.*' },
 };
+
+export const globalTypes = {
+  theme: {
+    name: 'Theme',
+    description: 'Global theme for components',
+    defaultValue: 'light',
+    toolbar: {
+      icon: 'lightning',
+      items: [
+        {
+          value: 'light',
+          title: 'Light',
+          right: '☀️',
+        },
+        {
+          value: 'dark',
+          title: 'Dark',
+          right: '🌑',
+        },
+      ],
+    },
+  },
+};
+
+export const decorators = [
+  (Story, ctx) => {
+    const { theme } = ctx.globals;
+
+    React.useEffect(() => {
+      document.querySelector('html').className = theme;
+    }, [theme]);
+
+    return <Story></Story>;
+  },
+];
