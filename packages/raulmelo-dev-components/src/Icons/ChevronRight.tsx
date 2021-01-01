b129967d61d03b47f57aeae3dcd418564f2df683
Raul Melo <melo.raulf@gmail.com>
2021-01-01 13:15:58 +0100

    feat(component): add ChevronRightIcon

---
+++ packages/raulmelo-dev-components/src/Icons/ChevronRight.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { ChevronDownIcon } from './ChevronDown';
+import { IconProps } from './types';
+
+export const ChevronRightIcon = (props: IconProps) => {
+  return <ChevronDownIcon style={{ transform: 'rotate(-90deg)' }} {...props} />;
+};
