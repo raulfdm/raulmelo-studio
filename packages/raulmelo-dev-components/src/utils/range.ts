362806dad4e3903b36aa50cb5cc567f13d3beb13
Raul Melo <melo.raulf@gmail.com>
2021-01-31 17:38:36 +0100

    chore(internals): add range util function

---
+++ packages/raulmelo-dev-components/src/utils/range.ts
@@ -0,0 +1,5 @@
+// https://dev.to/namirsab/comment/2050
+export function range(start: number, end: number) {
+  const length = end - start + 1;
+  return Array.from({ length }, (_, i) => start + i);
+}
