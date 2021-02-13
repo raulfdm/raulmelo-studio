3f77b83f0b2e5f715d721bf8fbc66ccc0a2ba1fc
Raul Melo <melo.raulf@gmail.com>
2021-02-13 09:42:01 +0100

    feat: replace Merriweather with ibm plex serif

--- apps/raulmelo-dev-web/src/pages/_document.tsx
+++ apps/raulmelo-dev-web/src/pages/_document.tsx
@@ -9,7 +9,7 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Merriweather:wght@400;700&family=Open+Sans:wght@300;400;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=IBM+Plex+Serif:ital,wght@0,400;0,700;1,400&family=Open+Sans:wght@300;400;700&display=swap"
             rel="stylesheet"
           />
           {/* Global Theme handler
