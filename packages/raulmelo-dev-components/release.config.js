807469d6ddd7abc00ce807e7fc46e053c04955ab
Raul Melo <melo.raulf@gmail.com>
2021-02-22 18:15:31 +0100

    Merge pull request #86 from raulfdm/develop
    deploy

--- packages/raulmelo-dev-components/release.config.js
+++ packages/raulmelo-dev-components/release.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   branches: ['main'],
   plugins: [
-    '@semantic-release/commit-analyzer',
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        releaseRules: [{ type: 'chore', scope: 'deps', release: 'minor' }],
+      },
+    ],
     '@semantic-release/release-notes-generator',
     '@semantic-release/npm',
     '@semantic-release/git',
