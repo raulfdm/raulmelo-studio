ccd97b2d67428181be911e02ff192439fc1ff5aa
Raul Melo <melo.raulf@gmail.com>
2021-03-07 12:26:27 +0100

    chore: resolve regeneratorRuntime in a more elegant way

--- packages/lambda-functions-collections/.babelrc.js
+++ packages/lambda-functions-collections/.babelrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   presets: ["@babel/preset-typescript", "@babel/preset-env"],
   plugins: [
+    "@babel/plugin-transform-runtime",
     "@babel/plugin-proposal-class-properties",
     "@babel/plugin-transform-object-assign",
     "@babel/plugin-proposal-object-rest-spread",
