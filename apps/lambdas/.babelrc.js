c4b1d2bfbc9f5e1fe54038af201cf38795166ed5
Raul Melo <melo.raulf@gmail.com>
2021-03-18 07:04:34 +0100

    chore: create a central babel config to be extended

--- apps/lambdas/.babelrc.js
+++ apps/lambdas/.babelrc.js
@@ -1,9 +1,3 @@
 module.exports = {
-  presets: ["@babel/preset-typescript", "@babel/preset-env"],
-  plugins: [
-    "@babel/plugin-transform-runtime",
-    "@babel/plugin-proposal-class-properties",
-    "@babel/plugin-transform-object-assign",
-    "@babel/plugin-proposal-object-rest-spread",
-  ],
+  extends: '../../config/.babelrc.js',
 };
