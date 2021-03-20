f58572fdb8b74a58561ae3240a979b1894e08243
Raul Melo <melo.raulf@gmail.com>
2021-03-20 21:00:25 +0100

    ci: remove sensitive info

--- apps/lambdas/scripts/deploy.sh
+++ apps/lambdas/scripts/deploy.sh
@@ -2,7 +2,4 @@
 
 echo "DEPLOY Functions"
 
-export NETLIFY_AUTH_TOKEN=1BDm-c764_ZV0nFPwiSlwT1yJi5vGqkc7Z3JhEErIqM
-export NETLIFY_SITE_ID=4b1b2f1e-e403-4c46-85e5-634161755f7c
-
 netlify deploy --functions=functions --dir=functions --prod
