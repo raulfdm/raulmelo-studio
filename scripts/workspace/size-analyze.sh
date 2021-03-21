528a56b5eee944369ad8d5c3582cd9c4803cdde7
Raul Melo <melo.raulf@gmail.com>
2021-03-21 08:11:42 +0100

    ci: add sizes pipeline and scripts

---
+++ scripts/workspace/size-analyze.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+echo "Opening bundle chart analyzer..."
+scripts/workspace/size.sh --why
---
+++ scripts/workspace/size.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+echo "Checking lib size....."
+yarn size-limit $1
