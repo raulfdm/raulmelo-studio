3026a11bebf08e510480effcef557f6384f979c0
Raul Melo <melo.raulf@gmail.com>
2021-03-21 09:22:59 +0100

    chore: add git config to publish packages

--- scripts/workspace/release.sh
+++ scripts/workspace/release.sh
@@ -2,4 +2,8 @@
 
 echo "┏━━━ 🚢 Release: lerna publish packages  ━━━━━━━"
 
+# Setup git globals
+git config --global user.email "melo.raulf@gmail.com"
+git config --global user.name "Raul Melo"
+
 yarn lerna publish --conventional-commits --no-private -y
