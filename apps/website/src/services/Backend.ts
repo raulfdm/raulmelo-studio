bee12b31df3d627892d62cfcd965be7d65551448
Raul Melo <melo.raulf@gmail.com>
2021-03-25 20:16:13 +0100

    refactor(website): remove unused code and dependency

--- apps/website/src/services/Backend.ts
+++ apps/website/src/services/Backend.ts
@@ -1,7 +1,4 @@
-import { Endpoints } from '@types-api';
-import { stringify as qsStringify } from 'qs';
-
-import { API_URL, IS_DEVELOPMENT } from '@config/app';
+import { API_URL } from '@config/app';
 
 async function fetcher(url: string, opts?: RequestInit) {
   const res = await fetch(url, opts);
@@ -9,42 +6,7 @@ async function fetcher(url: string, opts?: RequestInit) {
   return await res.json();
 }
 
-type HashObject = {
-  [param: string]: string;
-};
-
-type BackendOptions = {
-  path?: string;
-  params?: HashObject;
-};
-
 export const Backend = {
-  fetch(
-    endpoint: Endpoints,
-    options?: BackendOptions,
-  ): ReturnType<typeof fetcher> {
-    let url = `${API_URL}/${endpoint}`;
-
-    if (options?.path) {
-      url += options.path;
-    }
-
-    if (options?.params) {
-      const queryString = qsStringify(options.params, {
-        addQueryPrefix: true,
-        encode: false,
-      });
-
-      url += queryString;
-    }
-
-    if (IS_DEVELOPMENT) {
-      console.log('Fetching data from:', url);
-    }
-
-    return fetcher(url);
-  },
-
   async graphql<T>(query: string): Promise<T> {
     const url = `${API_URL}/graphql`;
 
