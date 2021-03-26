5b9862eaa7fa56a0eafd84ffed0bcf7848ee724b
Raul Melo <melo.raulf@gmail.com>
2021-03-26 20:27:09 +0100

    feat(website): replace purgecss config with tailwind-jit

--- apps/website/tailwind.config.js
+++ apps/website/tailwind.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   presets: [require('@raulfdm/blog-tailwind-preset')],
+  purge: [
+    './src/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@raulfdm/blog-components/dist/**',
+  ],
 };
