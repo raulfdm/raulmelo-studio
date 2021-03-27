e266946cd4ea6d1eb549c7077ab8ba6ed473d4fc
Raul Melo <melo.raulf@gmail.com>
2021-03-27 22:41:12 +0100

    fix(website): correct purge

--- apps/website/tailwind.config.js
+++ apps/website/tailwind.config.js
@@ -2,6 +2,6 @@ module.exports = {
   presets: [require('@raulfdm/blog-tailwind-preset')],
   purge: [
     './src/**/*.{js,ts,jsx,tsx}',
-    './node_modules/@raulfdm/blog-components/dist/**',
+    '../../node_modules/@raulfdm/blog-components/dist/**',
   ],
 };
