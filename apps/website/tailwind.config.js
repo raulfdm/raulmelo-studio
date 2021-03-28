3e4b6896e1c1f6915ee9f055da770a43fdfd9550
Raul Melo <melo.raulf@gmail.com>
2021-03-28 11:29:14 +0200

    fix: attempt to fix correct node_modules path to evaluate

--- apps/website/tailwind.config.js
+++ apps/website/tailwind.config.js
@@ -1,7 +1,9 @@
+const path = require('path');
+const blogCompPath = require.resolve('@raulfdm/blog-components');
+
+const blogCompFolderPath = path.dirname(blogCompPath);
+
 module.exports = {
   presets: [require('@raulfdm/blog-tailwind-preset')],
-  purge: [
-    './src/**/*.{js,ts,jsx,tsx}',
-    '../../node_modules/@raulfdm/blog-components/dist/**',
-  ],
+  purge: ['./src/**/*.{js,ts,jsx,tsx}', `${blogCompFolderPath}/**/*`],
 };
