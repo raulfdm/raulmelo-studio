05d8fcdd5435d4491b4f8a109d1229708a806ab8
Raul Melo <melo.raulf@gmail.com>
2021-03-30 21:17:41 +0200

    chore(blog-components): add support to postcss 8

--- packages/blog-components/.storybook/main.js
+++ packages/blog-components/.storybook/main.js
@@ -1,6 +1,17 @@
 module.exports = {
   stories: ['../src/**/*.stories.@(ts|tsx|js|jsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
+  ],
   // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
   typescript: {
     check: true, // type-check stories during Storybook build
