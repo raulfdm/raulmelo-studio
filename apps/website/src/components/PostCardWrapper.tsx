97ec976b426182dae786b6c734c9920098583a24
Raul Melo <melo.raulf@gmail.com>
2021-04-03 11:10:13 +0200

    fix(website): use next/image on postcard

--- apps/website/src/components/PostCardWrapper.tsx
+++ apps/website/src/components/PostCardWrapper.tsx
@@ -2,6 +2,7 @@ import { useLocalization } from '@hooks/useLocalization';
 import { PostCard } from '@raulfdm/blog-components';
 import { SupportedLanguages } from '@types-app';
 import { getPostUrl, getTagUrl } from '@utils/url';
+import Image from 'next/image';
 import Link from 'next/link';
 
 export type PostCardWrapperProps = {
@@ -29,6 +30,7 @@ export function PostCardWrapper({ post }: PostCardWrapperProps) {
       title={post.title}
       subtitle={post.subtitle}
       Link={Link}
+      renderImage={(props) => <Image {...props} />}
       titleLinkProps={{ locale: post.language }}
       postUrl={getPostUrl(post.slug)}
       publishDate={formatDate(new Date(post.date), {
