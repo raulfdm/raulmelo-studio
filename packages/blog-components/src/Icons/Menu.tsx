eac9820e0f02ab3b00dcaff0430e1a5ebf2d82a2
Raul Melo <melo.raulf@gmail.com>
2021-04-05 20:11:32 +0200

    refactor(blog-components): replace menu icon

--- packages/blog-components/src/Icons/Menu.tsx
+++ packages/blog-components/src/Icons/Menu.tsx
@@ -4,17 +4,19 @@ import { IconProps } from './types';
 export const MenuIcon = (props: IconProps) => {
   return (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
+      width={32}
+      height={32}
+      viewBox="0 0 32 32"
       fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
       <path
+        d="M5 16h22M5 8h22M5 24h22"
+        stroke="currentColor"
+        strokeWidth={2}
         strokeLinecap="round"
         strokeLinejoin="round"
-        strokeWidth={2}
-        d="M4 6h16M4 12h16m-7 6h7"
       />
     </svg>
   );
