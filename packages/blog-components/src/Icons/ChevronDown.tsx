e1f30d21b848760bff772668908bf1d84b4e9ea2
Raul Melo <melo.raulf@gmail.com>
2021-04-05 20:18:04 +0200

    refactor(blog-components): replace chevronDown icon

--- packages/blog-components/src/Icons/ChevronDown.tsx
+++ packages/blog-components/src/Icons/ChevronDown.tsx
@@ -4,17 +4,19 @@ import { IconProps } from './types';
 export const ChevronDownIcon = (props: IconProps) => {
   return (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
+      width={32}
+      height={32}
+      viewBox="0 0 32 32"
       fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
       <path
+        d="M26 12L16 22 6 12"
+        stroke="currentColor"
+        strokeWidth={2}
         strokeLinecap="round"
         strokeLinejoin="round"
-        strokeWidth={2}
-        d="M19 9l-7 7-7-7"
       />
     </svg>
   );
