a30efeb50afe66c849e7c9df004661651a99cff8
Raul Melo <melo.raulf@gmail.com>
2021-04-05 20:05:21 +0200

    refactor(blog-components): replace linkedIn icon

--- packages/blog-components/src/Icons/LinkedIn.tsx
+++ packages/blog-components/src/Icons/LinkedIn.tsx
@@ -4,17 +4,28 @@ import { IconProps } from './types';
 export const LinkedInIcon = (props: IconProps) => {
   return (
     <svg
-      viewBox="0 0 24 24"
-      height="21"
-      width="21"
-      aria-hidden="true"
-      focusable="false"
-      fill="currentColor"
+      width={32}
+      height={32}
+      viewBox="0 0 32 32"
+      fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <circle cx="4.983" cy="5.009" r="2.188"></circle>
-      <path d="M9.237 8.855v12.139h3.769v-6.003c0-1.584.298-3.118 2.262-3.118 1.937 0 1.961 1.811 1.961 3.218v5.904H21v-6.657c0-3.27-.704-5.783-4.526-5.783-1.835 0-3.065 1.007-3.568 1.96h-.051v-1.66H9.237zm-6.142 0H6.87v12.139H3.095z"></path>
+      <path
+        d="M26 5H6a1 1 0 00-1 1v20a1 1 0 001 1h20a1 1 0 001-1V6a1 1 0 00-1-1zM15 14v8M11 14v8"
+        stroke="currentColor"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M15 17.5a3.5 3.5 0 117 0V22"
+        stroke="currentColor"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path d="M11 11.5a1.5 1.5 0 100-3 1.5 1.5 0 000 3z" fill="currentColor" />
     </svg>
   );
 };
