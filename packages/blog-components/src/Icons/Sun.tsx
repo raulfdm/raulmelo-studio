4573dc8363117469db297afdaca2d1599965a1d6
Raul Melo <melo.raulf@gmail.com>
2021-04-05 20:06:26 +0200

    refactor(blog-components): replace sun icon

--- packages/blog-components/src/Icons/Sun.tsx
+++ packages/blog-components/src/Icons/Sun.tsx
@@ -4,15 +4,19 @@ import { IconProps } from './types';
 export const SunIcon = (props: IconProps) => {
   return (
     <svg
+      width={32}
+      height={32}
+      viewBox="0 0 32 32"
+      fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 20 20"
-      fill="currentColor"
       {...props}
     >
       <path
-        fillRule="evenodd"
-        d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
-        clipRule="evenodd"
+        d="M16 23.5a7.5 7.5 0 100-15 7.5 7.5 0 000 15zM16 4.5v-1M7.868 7.868l-.707-.707M4.5 16h-1M7.868 24.132l-.707.707M16 27.5v1M24.132 24.132l.707.707M27.5 16h1M24.132 7.868l.707-.707"
+        stroke="currentColor"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
