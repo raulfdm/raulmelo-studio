a3c34a761a3ae184ad126daf91c6bd8cc4d11dfd
Raul Melo <melo.raulf@gmail.com>
2021-04-05 20:03:28 +0200

    fix(blog-components): set default height and width

--- packages/blog-components/src/Icons/Moon.tsx
+++ packages/blog-components/src/Icons/Moon.tsx
@@ -4,6 +4,8 @@ import { IconProps } from './types';
 export const MoonIcon = (props: IconProps) => {
   return (
     <svg
+      width={32}
+      height={32}
       viewBox="0 0 32 32"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
