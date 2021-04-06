d130c68e267553e9c7ec0ee377446bfc5847df48
Raul Melo <melo.raulf@gmail.com>
2021-04-06 20:52:38 +0200

    fix: organize first part

--- apps/website/src/pages/cv.tsx
+++ apps/website/src/pages/cv.tsx
@@ -1,6 +1,8 @@
+import { MenuBar } from '@components/MenuBar';
 import { SEO } from '@components/SEO';
+import { sharedClasses } from '@components/uiClasses';
 import classNames from 'classnames';
-import Link from 'next/link';
+import Image from 'next/image';
 import React from 'react';
 
 const CurriculumPage = () => {
@@ -13,87 +15,97 @@ const CurriculumPage = () => {
         url="/cv"
       />
 
-      <div className="w-full iframe-container bg-gray-50 text-black min-h-full">
-        <Link href="/">
-          <a className="underline absolute left-3 top-3 print:hidden z-20">
-            Back to home
-          </a>
-        </Link>
+      {/* <div className="w-full iframe-container bg-gray-50 text-black min-h-full"> */}
+      <MenuBar />
+      <main
+        className={classNames([
+          sharedClasses.sectionContainer,
+          'pb-0',
+          'flex',
+          'justify-center md:justify-between',
+          'items-center md:items-start',
+          'flex-wrap',
+        ])}
+      >
+        <header className={classNames(['w-full md:w-[54%]', 'leading-normal'])}>
+          <h1 className="text-2xl md:text-3xl lg:text-4xl font-black">
+            Curriculum
+          </h1>
+          <p className="mb-4 text-base md:text-md lg:text-lg">
+            This CV is a live version from my Google Docs curriculum. In other
+            words, it&apos;s live updated and contains the most updated version
+            of it.
+          </p>
+          <p className="mb-4 text-base md:text-md lg:text-lg">
+            If you want to get a printable version, please{' '}
+            <a
+              href="https://docs.google.com/document/d/1xk0ChmPckqW85xtM1Hizx2tVbo2B61xjcpz-_dAH3f8/edit?usp=sharing"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="underline doc-link"
+            >
+              access the real document
+              <svg
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="prefix__StyledIconBase-ea9ulj-0 prefix__jZGNBW w-4 inline-block mb-2"
+              >
+                <title>{'LinkExternal icon'}</title>
+                <path d="M13 3l3.293 3.293-7 7 1.414 1.414 7-7L21 11V3z" />
+                <path d="M19 19H5V5h7l-2-2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2h14c1.103 0 2-.897 2-2v-5l-2-2v7z" />
+              </svg>
+            </a>{' '}
+            and select print option.
+          </p>
+        </header>
 
-        <article
+        <div
           className={classNames([
-            'container',
-            'mx-auto max-w-screen-md 1/5xl:max-w-screen-xl 2xl:max-w-max',
-            'pt-6 sm:pt-4',
-            '1/5xl:grid 1/5xl:grid-cols-3 1/5xl:gap-6',
+            'h-[390px] lg:h-[529px]',
+            ' w-full max-w-[280px] lg:max-w-[380px]',
+            'shadow-md',
+            'bg-white',
+            'relative',
+            'grid place-items-center',
+            'mt-6 md:ml-6 md:mt-0',
           ])}
         >
-          <header className="mb-8 pt-14 md:pt-5 px-4 md:px-0">
-            <h1 className="text-center text-5xl font-bold mb-4">Curriculum</h1>
-            <p>
-              This CV is a live version from my Google Docs curriculum. In other
-              words, it&apos;s live updated and contains the most updated
-              version of it.
-            </p>
-            <p>
-              If you want to get a printable version, please{' '}
-              <a
-                href="https://docs.google.com/document/d/1xk0ChmPckqW85xtM1Hizx2tVbo2B61xjcpz-_dAH3f8/edit?usp=sharing"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="underline doc-link"
-              >
-                access the real document
-                <svg
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="prefix__StyledIconBase-ea9ulj-0 prefix__jZGNBW"
-                >
-                  <title>{'LinkExternal icon'}</title>
-                  <path d="M13 3l3.293 3.293-7 7 1.414 1.414 7-7L21 11V3z" />
-                  <path d="M19 19H5V5h7l-2-2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2h14c1.103 0 2-.897 2-2v-5l-2-2v7z" />
-                </svg>
-              </a>{' '}
-              and select print option.
-            </p>
-          </header>
-
-          <main className="pb-6 1/5xl:col-span-2">
-            <iframe
-              src="https://docs.google.com/document/d/e/2PACX-1vRH5F5mV58PwToU2intAbHK7XujvdPyOhWr2gDdCC9YcisCSaJVctuGlzE_28zgEbJt4qEo-CUJl-hb/pub?embedded=true"
-              className="max-h-full w-full shadow-xl"
+          <button
+            className={classNames([
+              'bg-gray-600',
+              'rounded-md',
+              'py-4 px-6',
+              'z-20',
+              'relative',
+            ])}
+          >
+            Click here to expand
+          </button>
+          <figure
+            className={classNames([
+              'absolute',
+              'w-full',
+              'h-full',
+              'top-0 right-0 left-0 bottom-0',
+              'z-10',
+            ])}
+            style={{
+              filter: 'blur(1px)',
+            }}
+          >
+            <Image
+              src="https://res.cloudinary.com/duzei21zt/image/upload/v1617732749/site/cv_cover_dbad9dd714.png"
+              alt="cv cover"
+              layout="fill"
             />
-          </main>
-        </article>
-      </div>
-      <style jsx global>{`
-        #__next {
-          height: 100%;
-        }
-
-        article {
-          max-width: 815px;
-        }
-
-        .doc-link svg {
-          width: 16px;
-          display: inline-block;
-          margin-bottom: 10px;
-        }
-
-        .iframe-container iframe {
-          height: 75vh;
-        }
-
-        @media (max-width: 1440px) {
-        }
-
-        @media (min-width: 1440px) {
-          .iframe-container iframe {
-            height: 95vh;
-          }
-        }
-      `}</style>
+          </figure>
+        </div>
+      </main>
+      <iframe
+        style={{ height: 0 }}
+        src="https://docs.google.com/document/d/e/2PACX-1vRH5F5mV58PwToU2intAbHK7XujvdPyOhWr2gDdCC9YcisCSaJVctuGlzE_28zgEbJt4qEo-CUJl-hb/pub?embedded=true"
+        className="max-h-full w-full shadow-xl"
+      />
     </>
   );
 };
