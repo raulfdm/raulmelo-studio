79adf00b1c36b11908b97de0b0ef20e651ff28a4
Raul Melo <melo.raulf@gmail.com>
2021-04-08 13:35:27 +0200

    refactor: dot divider

--- packages/blog-components/src/DotDivider/DotDivider.tsx
+++ packages/blog-components/src/DotDivider/DotDivider.tsx
@@ -1,24 +1,63 @@
-import React from 'react';
 import styled from '@emotion/styled';
+import classNames from 'classnames';
+import React from 'react';
 
-const Hr = styled.hr`
-  && {
-    border-color: transparent;
-    position: relative;
-    overflow: visible;
-    text-align: center;
+const Dot = styled.div`
+  --hr-color: currentColor;
+  background-color: var(--hr-color);
 
-    &::before {
-      content: '...';
-      letter-spacing: 0.6em;
-      position: relative;
-      top: -26px;
-    }
+  /* IE and Legacy Edge */
+  @media screen and (-ms-high-contrast: active) {
+    --hr-color: windowText;
+  }
+  /* Using the new standards for forced colors, currently supported in Chromium Edge */
+  @media screen and (forced-colors: active) {
+    --hr-color: CanvasText;
+  }
+
+  /* For all variants, we need this: */
+  & + * {
+    margin-top: 0;
   }
 `;
 
-export const DotDivider = (props: DotDividerProps) => (
-  <Hr className="italic text-2xl my-5" {...props} data-testid="dot-divider" />
-);
+/**
+ * I'm currently use 3 sizes of tailwind typography:
+ * - lg => lower than md:
+ * - xl => md:
+ * - 2xl => lg:
+ *
+ * Since I'm fully replacing `hr` element with this custom divider,
+ * I have to specify the margins and sizes based on that.
+ */
 
-export type DotDividerProps = React.ComponentPropsWithoutRef<'hr'>;
+export const DotDivider = () => {
+  const dotClasses = classNames([
+    /**
+     * Arbitrary numbers based on the look and feel
+     */
+    'w-1.5 h-1.5 lg:w-[0.475rem] lg:h-[0.475rem]',
+    'rounded-full',
+  ]);
+
+  return (
+    <div
+      role="separator"
+      className={classNames([
+        'flex',
+        'justify-center',
+        'space-x-3',
+        /**
+         * - lg: margin top and bottom `em(56px/18px [base font]) => 3.111em`
+         * - xl: margin top and bottom `em(56px/20px [base font]) => 2.800em`
+         * - 2xl: margin top and bottom `em(72px/24px [base font]) => 3.000em`
+         */
+        'my-[3.111em] md:my-[2.8em] lg:my-[3em]',
+      ])}
+    >
+      <Dot aria-hidden className={classNames(dotClasses)} />
+      <Dot aria-hidden className={classNames(dotClasses)} />
+      <Dot aria-hidden className={classNames(dotClasses)} />
+    </div>
+  );
+};
