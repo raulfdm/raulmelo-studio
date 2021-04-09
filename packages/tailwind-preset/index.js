64a66818bc297ef644e6190c212dcbcdb0d817ac
Raul Melo <melo.raulf@gmail.com>
2021-04-09 18:39:14 +0200

    refactor(blog-tailwind-preset): revamp base styles
    Also add aspect-ratio plugin

--- packages/tailwind-preset/index.js
+++ packages/tailwind-preset/index.js
@@ -1,4 +1,5 @@
 const colors = require('tailwindcss/colors');
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   future: {
@@ -8,92 +9,79 @@ module.exports = {
   purge: false,
   darkMode: 'class',
   theme: {
+    fontSize: {
+      xs: ['0.563rem', { lineHeight: 1.3 }],
+      sm: ['0.75rem', { lineHeight: 1.3 }],
+      base: ['1rem', { lineHeight: 1.3 }],
+      md: ['1.125rem', { lineHeight: 1.3 }],
+      lg: ['1.333rem', { lineHeight: 1.3 }],
+      xl: ['1.777rem', { lineHeight: 1.3 }],
+      '2xl': ['2.369rem', { lineHeight: 1.3 }],
+      '3xl': ['3.157rem', { lineHeight: 1.3 }],
+      '4xl': ['4.209rem', { lineHeight: 1.3 }],
+    },
     fontFamily: {
-      sans: `"Open Sans", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, Open Sans, "Helvetica Neue", sans-serif`,
-      serif: `"IBM Plex Serif", Georgia, Cambria, Times New Roman, Times, serif`,
-      title: `"content-title", Georgia, Cambria, "Times New Roman", Times, serif`,
+      sans: ['"proxima-nova"', ...defaultTheme.fontFamily.sans],
     },
     extend: {
       colors: {
         blue: colors.blueGray,
+        black: '#2c2c2c',
       },
       textColor: {
-        black: 'rgba(0,0,0,0.84)',
+        black: '#2c2c2c',
       },
       screens: {
-        print: { raw: 'print' },
-        '1/5xl': '1440px',
+        'ipad-pro': '834px',
       },
       transitionProperty: {
         spacing: 'margin, padding',
         theme: 'background-color, color, fill, opacity, border, shadow',
       },
-      borderWidth: {
-        10: '10px',
-      },
-      minWidth: {
-        '1/2': '50%',
-      },
       typography: (theme) => {
         const white = theme('textColor.white');
         const black = theme('textColor.black');
-        const serif = theme('fontFamily.serif');
-        const sans = theme('fontFamily.sans');
 
         return {
+          /**
+           * The defaults will be applied for both Light and Dark theme
+           */
           DEFAULT: {
             css: {
               color: black,
-              fontFamily: serif,
-              h1: {
-                fontFamily: serif,
-              },
-              h2: {
-                fontFamily: sans,
-              },
-              h3: {
-                fontFamily: sans,
-              },
-              h4: {
-                fontFamily: sans,
-              },
-              h5: {
-                fontFamily: sans,
-              },
-              h6: {
-                fontFamily: sans,
-              },
               blockquote: {
                 borderLeftColor: black,
+                fontWeight: 900,
+              },
+              h1: {
+                fontWeight: 800,
+                color: black,
+              },
+              h2: {
+                fontWeight: 800,
+                color: black,
+              },
+              h3: {
+                fontWeight: 800,
+                color: black,
+              },
+              h4: {
+                fontWeight: 800,
+                color: black,
+              },
+              h5: {
+                fontWeight: 800,
+                color: black,
+              },
+              h6: {
+                fontWeight: 800,
+                color: black,
               },
             },
           },
-          lg: {
-            css: {
-              hr: {
-                overflow: 'visible',
-                marginBottom: '2.6rem',
-                marginTop: '2.6rem',
-              },
-            },
-          },
-          xl: {
-            css: {
-              hr: {
-                overflow: 'visible',
-                marginBottom: '3.6rem',
-                marginTop: '3.6rem',
-              },
-            },
-          },
-          '2xl': {
-            css: {
-              /**
-               * 20px
-               */
-              fontSize: '1.25rem',
-            },
-          },
+          /**
+           * Dark more overrides
+           */
           dark: {
             css: {
               color: white,
@@ -148,5 +136,8 @@ module.exports = {
     },
     typography: ['dark', 'responsive'],
   },
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 };
