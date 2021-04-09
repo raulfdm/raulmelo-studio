204343e14b6f651d779506b9eae74e2702edd7d1
Raul Melo <melo.raulf@gmail.com>
2021-04-09 12:15:25 +0200

    fix: 404 page

--- apps/website/src/pages/404.tsx
+++ apps/website/src/pages/404.tsx
@@ -19,20 +19,24 @@ const Error = () => {
         <title>{formatMessage(pageTitleMessage)}</title>
       </Head>
       <MenuBar />
-      <main className="mx-auto max-w-screen-lg w-8/12 text-center">
-        <h2
+      <main className="grid-container">
+        <h1
           className={classNames([
-            'text-6xl md:text-7xl lg:text-8xl xl:text-9xl',
-            'font-bold font-serif',
+            'text-[3.5rem] md:text-[4.5rem] lg:text-[6rem]',
+            'font-black',
+            'text-center',
+            'col-span-full',
           ])}
         >
-          Oops!
-        </h2>
+          Oops! <span className="text-base">404</span>
+        </h1>
+
         <p
           className={classNames([
             'mt-3',
-            'text-2xl md:text-4xl lg:text-5xl',
-            'font-bold font-serif',
+            'text-2xl md:text-3xl',
+            'font-extra',
+            'col-span-full md:col-start-1 lg:col-start-3 lg:col-end-11',
           ])}
         >
           <FormattedMessage id="404.subtitle" />
@@ -40,8 +44,8 @@ const Error = () => {
         <p
           className={classNames([
             'mt-5',
-            'text-gray-800 dark:text-gray-300',
             'text-lg',
+            'col-span-full md:col-start-1 lg:col-start-3 lg:col-end-11',
           ])}
         >
           <FormattedMessage id="404.description" />
