8f682bd79ccb50bc9e425deb2f0708453b24c95e
Raul Melo <melo.raulf@gmail.com>
2021-04-10 12:09:01 +0200

    refactor: revamp all icons with the new aesthetic

--- packages/blog-components/src/Icons/Close.tsx
+++ packages/blog-components/src/Icons/Close.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import { IconProps } from './types';
 
-export const CloseIcon = (props: IconProps) => {
+export const CloseIcon = ({ width, ...props }: IconProps) => {
   return (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
+      width={width}
+      height={width}
+      viewBox="0 0 32 32"
       fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
       <path
+        d="M25 7L7 25M25 25L7 7"
+        stroke="currentColor"
+        strokeWidth={2}
         strokeLinecap="round"
         strokeLinejoin="round"
-        strokeWidth={2}
-        d="M6 18L18 6M6 6l12 12"
       />
     </svg>
   );
