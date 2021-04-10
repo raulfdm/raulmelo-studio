178728c424ad4d2884c79c96993c31b8ebdb4ddd
Raul Melo <melo.raulf@gmail.com>
2021-04-10 11:40:58 +0200

    ci: use lerna to run the release

--- scripts/workspace/release.sh
+++ scripts/workspace/release.sh
@@ -6,4 +6,4 @@ echo "┏━━━ 🚢 Release: lerna publish packages  ━━━━━━━"
 export GIT_AUTHOR_NAME="Raul Melo"
 export GIT_AUTHOR_EMAIL="melo.raulf@gmail.com"
 
-yarn workspaces run semantic-release -e semantic-release-monorepo
+lerna exec --concurrency 1 --scope='{mdx-prism-2,@raulfdm/blog-components,@raulfdm/blog-tailwind-preset}' -- semantic-release -e semantic-release-monorepo --tag-format='${LERNA_PACKAGE_NAME}-v\${version}'
