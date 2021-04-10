9045f2d270dfa5b86076b0fe2f85493f2f8495f0
Raul Melo <melo.raulf@gmail.com>
2021-04-10 11:19:03 +0200

    ci: attempt of releasing packages with semantic release

---
+++ .releaserc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  extends: 'semantic-release-monorepo',
+  branches: ['main'],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        releaseRules: [{ type: 'chore', scope: 'deps', release: 'patch' }],
+      },
+    ],
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    '@semantic-release/git',
+    [
+      '@semantic-release/github',
+      {
+        assets: ['.'],
+      },
+    ],
+  ],
+};
