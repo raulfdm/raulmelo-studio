c29586c9311e7b510655daa9b8ef81039a88d6ad
Raul Melo <melo.raulf@gmail.com>
2021-04-10 19:34:35 +0200

    ci: fix semantic release pipeline stuck forever

--- scripts/workspace/release.sh
+++ scripts/workspace/release.sh
@@ -5,5 +5,8 @@ echo "┏━━━ 🚢 Release: lerna publish packages  ━━━━━━━"
 # Setup git globals
 export GIT_AUTHOR_NAME="Raul Melo"
 export GIT_AUTHOR_EMAIL="melo.raulf@gmail.com"
+# This prevents semantic release being stuck forever
+# https://github.com/semantic-release/git/issues/154#issuecomment-524676502
+export HUSKY_SKIP_HOOKS=1
 
 lerna exec --concurrency 1 --scope='{mdx-prism-2,@raulfdm/blog-components,@raulfdm/blog-tailwind-preset}' -- semantic-release -e semantic-release-monorepo --tag-format='${LERNA_PACKAGE_NAME}@\${version}'
