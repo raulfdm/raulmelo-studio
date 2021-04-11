7d5a4752a53ec84d40fbf9383e0b0d51043faffb
Raul Melo <melo.raulf@gmail.com>
2021-04-11 18:36:14 +0200

    chore: migrate to mode "jit"

--- apps/website/postcss.config.js
+++ apps/website/postcss.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   plugins: {
-    '@tailwindcss/jit': {},
     'postcss-preset-env': {},
     autoprefixer: {},
   },
--- apps/website/tailwind.config.js
+++ apps/website/tailwind.config.js
@@ -5,5 +5,10 @@ const blogCompFolderPath = path.dirname(blogCompPath);
 
 module.exports = {
   presets: [require('@raulfdm/blog-tailwind-preset')],
+  mode: 'jit',
+  // purge: {
+  //   enable: true,
+  //   content: ['./src/**/*.{js,ts,jsx,tsx}', `${blogCompFolderPath}/**/*`],
+  // },
   purge: ['./src/**/*.{js,ts,jsx,tsx}', `${blogCompFolderPath}/**/*`],
 };
