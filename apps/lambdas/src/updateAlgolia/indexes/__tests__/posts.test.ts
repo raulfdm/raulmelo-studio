3d7ef34203641dc6a3e1eba505ff8b574068ef30
Raul Melo <melo.raulf@gmail.com>
2021-04-23 19:17:20 +0200

    test: add unit tests for post index updater

---
+++ apps/lambdas/src/updateAlgolia/indexes/__tests__/posts.test.ts
@@ -0,0 +1,52 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+import { AlgoliaObject } from '../../types';
+import { getPostsToAlgolia } from '../posts';
+
+jest.mock('../../../utils/api', () => {
+  const { postApi } = require('../__fixtures__/posts');
+
+  return {
+    api: {
+      query: jest.fn(() => Promise.resolve(postApi)),
+    },
+  };
+});
+
+describe('fn: getPostsToAlgolia', () => {
+  let indexName: string;
+  let data: AlgoliaObject[];
+
+  beforeEach(async () => {
+    [indexName, data] = await getPostsToAlgolia();
+  });
+
+  it('returns expected index name', async () => {
+    expect(indexName).toMatchInlineSnapshot(`"posts"`);
+  });
+
+  it('returns expected data structure', () => {
+    data.forEach((obj) => {
+      expect(obj).toEqual(
+        expect.objectContaining({
+          date: expect.any(String),
+          objectID: expect.any(String),
+          id: expect.any(String),
+          excerpt: expect.any(String),
+          language: expect.any(String),
+          title: expect.any(String),
+          subtitle: expect.any(String),
+          featured_image: expect.objectContaining({
+            width: expect.any(Number),
+            height: expect.any(Number),
+            url: expect.any(String),
+          }),
+        }),
+      );
+
+      // Can't send entire content
+      expect(obj).not.toEqual({
+        content: expect.anything(),
+      });
+    });
+  });
+});
