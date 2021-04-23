5d4b9df66db02f0394c5d3df9673e3dd8da584ed
Raul Melo <melo.raulf@gmail.com>
2021-04-23 18:38:15 +0200

    refactor: create config file with the settings

---
+++ apps/lambdas/src/config/index.ts
@@ -0,0 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
+
+export const SETTINGS = {
+  algolia: {
+    adminKey: process.env.ALGOLIA_ADMIN_KEY,
+    appId: process.env.ALGOLIA_APP_ID,
+  },
+  apiUrl: `${process.env.API_URL}/graphql`,
+};
