bb4fc6d78264d749b4372dfdfff6bb65d5a12ffb
Raul Melo <melo.raulf@gmail.com>
2021-04-23 22:28:32 +0200

    feat: move pushAlgoliaData to its own file

---
+++ apps/lambdas/src/updateAlgolia/pushAlgoliaData.ts
@@ -0,0 +1,17 @@
+import algolia from 'algoliasearch';
+import { SETTINGS } from '../config';
+import { AlgoliaObjectList } from './types';
+
+export async function pushAlgoliaData(
+  indexName: string,
+  data: AlgoliaObjectList,
+): Promise<void> {
+  if (!SETTINGS.algolia.appId || !SETTINGS.algolia.adminKey) {
+    throw new Error('App ID or Admin Key is missing');
+  }
+
+  const client = algolia(SETTINGS.algolia.appId, SETTINGS.algolia.adminKey);
+  const index = client.initIndex(indexName);
+
+  await index.saveObjects(data);
+}
