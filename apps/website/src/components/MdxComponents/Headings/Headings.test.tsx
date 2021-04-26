de3048e1db7a1c4e2a3744f9f771d8285c5676cb
Raul Melo <melo.raulf@gmail.com>
2021-04-26 08:18:43 +0200

    test: update snapshot

--- apps/website/src/components/MdxComponents/Headings/Headings.test.tsx
+++ apps/website/src/components/MdxComponents/Headings/Headings.test.tsx
@@ -36,34 +36,84 @@ describe('<Headings />', () => {
         --icon-visible: unset;
       }
 
+      .emotion-2.emotion-2 {
+        position: relative;
+        display: table;
+        --icon-visible: hidden;
+      }
+
+      .emotion-2.emotion-2:hover {
+        --icon-visible: unset;
+      }
+
+      .emotion-4.emotion-4 {
+        position: relative;
+        display: table;
+        --icon-visible: hidden;
+      }
+
+      .emotion-4.emotion-4:hover {
+        --icon-visible: unset;
+      }
+
+      .emotion-6.emotion-6 {
+        position: relative;
+        display: table;
+        --icon-visible: hidden;
+      }
+
+      .emotion-6.emotion-6:hover {
+        --icon-visible: unset;
+      }
+
+      .emotion-8.emotion-8 {
+        position: relative;
+        display: table;
+        --icon-visible: hidden;
+      }
+
+      .emotion-8.emotion-8:hover {
+        --icon-visible: unset;
+      }
+
+      .emotion-10.emotion-10 {
+        position: relative;
+        display: table;
+        --icon-visible: hidden;
+      }
+
+      .emotion-10.emotion-10:hover {
+        --icon-visible: unset;
+      }
+
       <div>
         <h1
-          class="emotion-0"
+          class="emotion-0 emotion-1"
         >
           Some weird Text
         </h1>
         <h2
-          class="emotion-0"
+          class="emotion-2 emotion-3"
         >
           Some weird Text
         </h2>
         <h3
-          class="emotion-0"
+          class="emotion-4 emotion-5"
         >
           Some weird Text
         </h3>
         <h4
-          class="emotion-0"
+          class="emotion-6 emotion-7"
         >
           Some weird Text
         </h4>
         <h5
-          class="emotion-0"
+          class="emotion-8 emotion-9"
         >
           Some weird Text
         </h5>
         <h6
-          class="emotion-0"
+          class="emotion-10 emotion-11"
         >
           Some weird Text
         </h6>
