a253a647d6d1a7cd44653c9b0936ef6880a205ae
Raul Melo <melo.raulf@gmail.com>
2021-04-27 21:24:07 +0200

    refactor: migrate ProseContainer to use twin.macro

--- apps/website/src/components/ProseContainer/ProseContainer.tsx
+++ apps/website/src/components/ProseContainer/ProseContainer.tsx
@@ -1,29 +1,7 @@
-import React from 'react';
-import classNames from 'classnames';
+import tw, { styled } from 'twin.macro';
 
-export const ProseContainer = ({
-  children,
-  className,
-  as = 'article',
-}: ProseContainerProps) => {
-  const Element = as;
-
-  return (
-    <Element
-      className={classNames([
-        'prose dark:prose-dark',
-        'prose-lg md:prose-xl lg:prose-2xl',
-        'max-w-full',
-        className,
-      ])}
-    >
-      {children}
-    </Element>
-  );
-};
-
-export type ProseContainerProps = {
-  children: React.ReactNode;
-  className?: string;
-  as?: React.ElementType;
-};
+export const ProseContainer = styled.article`
+  ${tw`prose dark:prose-dark`};
+  ${tw`prose-lg md:prose-xl lg:prose-2xl`};
+  ${tw`max-w-full`};
+`;
