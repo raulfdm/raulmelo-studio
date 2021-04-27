4a20d1bcb6ef3b57bd979a0bd1b37a72f257db08
Raul Melo <melo.raulf@gmail.com>
2021-04-27 11:48:23 +0200

    refactor: move "grid-container" out of css to twin.macro

---
+++ apps/website/src/styles/base.ts
@@ -0,0 +1,16 @@
+import tw from 'twin.macro';
+
+export const gridContainer = tw`
+  w-full
+  grid-cols-4
+  px-4
+  gap-4
+  grid
+  mx-auto
+
+  md:max-w-5xl
+  md:grid-cols-6
+
+  lg:max-w-7xl
+  lg:grid-cols-12
+`;
