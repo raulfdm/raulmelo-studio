ed0651c8b289bdd658fa8b7dd98c3ddc29dc3833
Raul Melo <melo.raulf@gmail.com>
2021-04-27 21:52:47 +0200

    refactor: migrate Blog to twin.macro

--- apps/website/src/screens/Blog/Blog.tsx
+++ apps/website/src/screens/Blog/Blog.tsx
@@ -1,11 +1,12 @@
-import { useLocalization } from '@hooks/useLocalization';
 import { Pagination, PaginationItem } from '@components/Pagination';
+import { useLocalization } from '@hooks/useLocalization';
 import { Posts } from '@screens/Home/components/Posts';
 import { usePageQueryReset } from '@screens/Home/hooks/usePageQueryReset';
 import { NextSeo } from 'next-seo';
 import { useRouter } from 'next/router';
 import React from 'react';
 import { defineMessages, FormattedMessage } from 'react-intl';
+import 'twin.macro';
 import { BlogGraphQLResponse } from './types';
 
 const messages = defineMessages({
@@ -40,10 +41,10 @@ export const Blog: React.FC<BlogProps> = ({
         description={formatMessage({ id: 'blog.description' })}
       />
 
-      <header className="col-span-full lg:col-span-10">
-        <h1 className="text-3xl md:text-4xl font-extrabold">Blog</h1>
+      <header tw="col-span-full lg:col-span-10">
+        <h1 tw="text-3xl md:text-4xl font-extrabold">Blog</h1>
 
-        <p className="col-span-full lg:col-span-10 text-md md:text-lg my-4">
+        <p tw="col-span-full lg:col-span-10 text-md md:text-lg my-4">
           <FormattedMessage id="blog.description" />
         </p>
       </header>
