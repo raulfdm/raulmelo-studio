7951fec929c7189bd4e786c5f04fe2b2c232cec7
Raul Melo <melo.raulf@gmail.com>
2021-04-28 20:25:26 +0200

    refactor: migrate Posts to twin.macro

--- apps/website/src/screens/Home/components/Posts/Posts.tsx
+++ apps/website/src/screens/Home/components/Posts/Posts.tsx
@@ -1,7 +1,7 @@
 import { PostCardWrapper } from '@components/PostCardWrapper';
 import { IHomePost } from '@screens/Home/types';
-import classNames from 'classnames';
 import { AnimatePresence, motion } from 'framer-motion';
+import tw from 'twin.macro';
 
 const itemsAnimationVariants = {
   visible: (index: number) => ({
@@ -22,25 +22,17 @@ type PostsProps = {
   title: string;
 };
 
+const styles = {
+  wrapper: tw`col-span-full`,
+  title: tw`font-sans font-extrabold text-lg lg:text-xl mb-4 lg:mb-8`,
+  list: tw`grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6`,
+};
+
 export const Posts = ({ posts, title }: PostsProps) => {
   return (
-    <section className="col-span-full">
-      <h2
-        className={classNames([
-          'font-sans font-extrabold',
-          'text-lg lg:text-xl',
-          'mb-4 lg:mb-8',
-        ])}
-      >
-        {title}
-      </h2>
-      <motion.ul
-        className={classNames([
-          'grid',
-          'grid-cols-1 md:grid-cols-2 lg:grid-cols-3',
-          'gap-6',
-        ])}
-      >
+    <section css={styles.wrapper}>
+      <h2 css={styles.title}>{title}</h2>
+      <motion.ul css={styles.list}>
         <AnimatePresence initial={false}>
           {posts
             ? posts.map((post) => (
