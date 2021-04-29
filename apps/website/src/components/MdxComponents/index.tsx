77af78db05887376aaff6b92a064df25f7c1d0a3
Raul Melo <melo.raulf@gmail.com>
2021-04-29 11:45:12 +0200

    chore: add lazy loading for MDX components

--- apps/website/src/components/MdxComponents/index.tsx
+++ apps/website/src/components/MdxComponents/index.tsx
@@ -1,34 +1,55 @@
-import NextImage from 'next/image';
-import { Tweet } from 'react-twitter-widgets';
+import dynamic from 'next/dynamic';
+
 import { BigQuote } from './BigQuote';
-import { CodePenIframe } from './CodePenIframe';
 import { DotDivider } from './DotDivider';
-import { Gif } from './Gif';
+
+import type { YouTubeIframe as YouTubeIframeType } from './YouTubeIframe';
+import type { Tweet as TweetType } from './Tweet';
+import type { ImageSlider as ImageSliderType } from './ImageSlider';
+import type { Gif as GifType } from './Gif';
+import type { CodePenIframe as CodePenIframeType } from './CodePenIframe';
+import type { BigQuote as BigQuoteType } from './BigQuote';
+
 import { Image } from './Image';
 import { H1, H2, H3, H4, H5, H6 } from './Headings';
-import { YouTubeIframe } from './YouTubeIframe';
-import { ImageSliderFactory } from './ImageSlider';
+
+const YouTubeVideo = dynamic(() =>
+  import('./YouTubeIframe').then((mod) => mod.YouTubeIframe),
+) as typeof YouTubeIframeType;
+
+const BigQuote = dynamic(() =>
+  import('./BigQuote').then((mod) => mod.BigQuote),
+) as typeof BigQuoteType;
+
+const Gif = dynamic(() =>
+  import('./Gif').then((mod) => mod.Gif),
+) as typeof GifType;
+
+const CodePenIframe = dynamic(() =>
+  import('./CodePenIframe').then((mod) => mod.CodePenIframe),
+) as typeof CodePenIframeType;
+
+const ImageSlider = dynamic(() =>
+  import('./ImageSlider').then((mod) => mod.ImageSlider),
+) as typeof ImageSliderType;
+
+const Tweet = dynamic(() =>
+  import('./Tweet').then((mod) => mod.Tweet),
+) as typeof TweetType;
 
 export const mdxComponents = {
   BigQuote,
   Gif,
   CodePen: CodePenIframe,
-  YouTubeVideo: YouTubeIframe,
+  YouTubeVideo,
   hr: DotDivider,
   Image,
-  Tweet: function TweetWrapper({ tweetId }: { tweetId: string }) {
-    return <Tweet tweetId={tweetId} options={{ align: 'center' }} />;
-  },
+  Tweet,
   h1: H1,
   h2: H2,
   h3: H3,
   h4: H4,
   h5: H5,
   h6: H6,
-  ImageSlider: ImageSliderFactory({
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    ImageComponent(props: any) {
-      return <NextImage {...props} layout="responsive" />;
-    },
-  }),
+  ImageSlider,
 };
