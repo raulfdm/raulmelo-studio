007c9428b59292828035731fb9ffcf5e09331b28
Raul Melo <melo.raulf@gmail.com>
2021-04-29 07:04:57 +0200

    refactor: migrate Header to twin.macro

--- apps/website/src/components/templates/MdxPost/components/Header/Header.tsx
+++ apps/website/src/components/templates/MdxPost/components/Header/Header.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import classNames from 'classnames';
 import { FormattedMessage } from 'react-intl';
+import tw from 'twin.macro';
 
 type HeaderProps = {
   title: string;
   publishedDate: string;
   subtitle?: string;
+};
+
+const styles = {
+  header: tw`border-b`,
+  title: tw`text-xl sm:text-2xl lg:text-3xl font-black tracking-tight`,
+  subtitle: tw`tracking-tight text-lg sm:text-xl lg:text-2xl font-medium opacity-80 mt-2`,
+  paragraph: tw`text-right text-base lg:text-md mt-6 mb-4`,
 };
 
 export const Header: React.FC<HeaderProps> = React.memo(function Header({
@@ -14,35 +21,10 @@ export const Header: React.FC<HeaderProps> = React.memo(function Header({
   publishedDate,
 }) {
   return (
-    <header className={classNames(['border-b'])}>
-      <h1
-        className={classNames([
-          'text-xl sm:text-2xl lg:text-3xl',
-          'font-black',
-          'tracking-tight',
-        ])}
-      >
-        {title}
-      </h1>
-      {subtitle && (
-        <p
-          className={classNames([
-            'tracking-tight',
-            'text-lg sm:text-xl lg:text-2xl',
-            'font-medium opacity-80',
-            'mt-2',
-          ])}
-        >
-          {subtitle}
-        </p>
-      )}
-      <p
-        className={classNames([
-          'text-right',
-          'text-base lg:text-md',
-          'mt-6 mb-4',
-        ])}
-      >
+    <header css={styles.header}>
+      <h1 css={styles.title}>{title}</h1>
+      {subtitle && <p css={styles.subtitle}>{subtitle}</p>}
+      <p css={styles.paragraph}>
         <FormattedMessage
           id="blogPost.publishedDate"
           values={{
