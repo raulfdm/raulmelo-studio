238cb66c4c303289db12e3922d051d9353770bdb
Raul Melo <melo.raulf@gmail.com>
2021-05-02 14:49:11 +0200

    fix: includes unist* packages in the final bundle

--- packages/mdx-prism-2/rollup.config.js
+++ packages/mdx-prism-2/rollup.config.js
@@ -1,5 +1,7 @@
 import pkg from './package.json';
 import commonJs from '@rollup/plugin-commonjs';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import json from '@rollup/plugin-json';
 
 export default {
   input: 'src/index.js',
@@ -9,6 +11,25 @@ export default {
     exports: 'default',
     sourcemap: true,
   },
-  plugins: [commonJs()],
-  external: Object.entries(pkg.dependencies).map(([name]) => name),
+  plugins: [json(), nodeResolve(), commonJs()],
+  external: Object.entries(pkg.dependencies)
+    .map(([name]) => name)
+    /**
+     * This includes in the bundle ONLY these 3 problematic packages.
+     *
+     * If use them as external, it'll throw an error:
+     *
+     * > [ERR_REQUIRE_ESM]: Must use import to load ES Module: /vercel/path0/node_modules/unist-util-filter/index.js
+     * > require() of ES modules is not supported.
+     *
+     * And that's because these 3 deps now are only ESM.
+     */
+    .filter(
+      (name) =>
+        ![
+          'unist-util-filter',
+          'unist-util-visit',
+          'unist-util-visit-parents',
+        ].includes(name),
+    ),
 };
