46f4ce44ac9bca200157216577fff31e929b7442
Raul Melo <melo.raulf@gmail.com>
2021-05-15 15:58:42 +0200

    refactor: generateRss code and enable for both pt and en

--- apps/website/scripts/generateRss.js
+++ apps/website/scripts/generateRss.js
@@ -4,75 +4,59 @@ const fetch = require('node-fetch');
 const fs = require('fs');
 const path = require('path');
 
-const URL = process.env.API_ENDPOINT ?? 'http://localhost:1337';
+const API_ENDPOINT = process.env.API_ENDPOINT ?? 'http://localhost:1337';
+const LOCALES_TO_GEN_RSS = ['en', 'pt'];
 
-if (URL.includes('localhost')) {
+if (API_ENDPOINT.includes('localhost')) {
   console.log('Getting data from Localhost');
 }
 
-const query = `
-query Content {
-  personalInformation {
-    full_name
-  }
-  defaultSeo {
-    title
-    description
-  }
-  site {
-    url
-  }
-  tils(locale: "en") {
-    slug
-    publishedAt
-    title
-    slug
-    description: title
-  }
-  posts(locale: "en") {
-    slug
-    publishedAt: createdAt
-    title
-    slug
-    description
-  }
-}
-`;
+(async function generateRssFeedIIFE() {
+  for await (const locale of LOCALES_TO_GEN_RSS) {
+    const { tils, posts, site, rss } = await getContent(locale);
 
-fetch(`${URL}/graphql`, {
-  method: 'POST',
-  body: JSON.stringify({ query }),
-  headers: { 'Content-Type': 'application/json' },
-})
-  .then((r) => r.json())
-  .then(({ data: { tils, posts, personalInformation, site } }) => {
-    console.log(personalInformation);
-    const content = [
-      ...tils.map((t) => ({
-        ...t,
-        category: 'Today I learned',
-        urlPrefix: 'til',
-      })),
-      ...posts.map((b) => ({ ...b, category: 'Post', urlPrefix: 'blog' })),
+    const rssConfig = {
+      ...rss,
+      url: site.url,
+      locale,
+    };
+
+    const allContent = [
+      ...tils.map(setUrlPrefix('til')),
+      ...posts.map(setUrlPrefix('blog')),
     ].sort(sortByPublishedDate);
 
-    return getRssXml(content, site.url);
-  })
-  .then(async (rssString) => {
-    fs.writeFileSync(path.resolve(__dirname, '../public/rss.xml'), rssString);
-    console.log('all good :)');
-  });
+    const rssString = getRssXml(allContent, rssConfig);
 
-function sortByPublishedDate(prevContent, nextContent) {
-  return (
-    Date.parse(nextContent.publishedAt) - Date.parse(prevContent.publishedAt)
-  );
-}
+    const filePath = writeFile(rssString, locale);
 
-function getRssXml(content, url) {
-  const { rssItemsXml, latestPostDate } = blogPostsRssXml(content);
+    console.log(`RSS Feed generated: "${filePath}"`);
+  }
 
-  // Edit the '<link>' and '<description>' data here to reflect your own website details!
+  function setUrlPrefix(prefix) {
+    return (c) => ({ ...c, urlPrefix: prefix });
+  }
+
+  function writeFile(rssString, locale) {
+    const fileName = locale === 'en' ? 'rss.xml' : 'rss-pt.xml';
+    const filePath = path.resolve(__dirname, '../public', fileName);
+
+    fs.writeFileSync(filePath, rssString);
+
+    return filePath;
+  }
+
+  function sortByPublishedDate(prevContent, nextContent) {
+    return (
+      Date.parse(nextContent.publishedAt) - Date.parse(prevContent.publishedAt)
+    );
+  }
+})();
+
+function getRssXml(content, rss) {
+  const rssItemsXml = content.map(getRssItem).join('');
+  const latestPostDate = humanizeDate(content[0].publishedAt);
+
   return `<?xml version="1.0" ?>
   <rss
     xmlns:dc="http://purl.org/dc/elements/1.1/"
@@ -81,62 +65,99 @@ function getRssXml(content, url) {
     version="2.0"
   >
     <channel>
-        <title><![CDATA[Raul Melo RSS Feed]]></title>
-        <link>${url}</link>
+        <title><![CDATA[${rss.title}]]></title>
+        <link>${rss.url}</link>
         <description>
-          <![CDATA[A description about your own website that really shows off what it's all about]]>
+          <![CDATA[${rss.description}]]>
         </description>
-        <language>en</language>
+        <language>${rss.locale}</language>
         <lastBuildDate>${latestPostDate}</lastBuildDate>
         ${rssItemsXml}
     </channel>
   </rss>`;
+
+  function getRssItem({ publishedAt, urlPrefix, description, slug, title }) {
+    const postHref = `${rss.url}/${urlPrefix}/${slug}`;
+    return `
+          <item>
+            <guid>${postHref}</guid>
+            <title><![CDATA[${title}]]></title>
+            <link>${postHref}</link>
+            <pubDate>${humanizeDate(publishedAt)}</pubDate>
+            <guid isPermaLink="false">${postHref}</guid>
+            
+            <description>
+            <![CDATA[${description}]]>
+            </description>
+            <content:encode>
+            <div style="width: 100%; margin: 0 auto; max-width: 800px; padding: 40px 40px;">
+              <p>
+                I've posted a new article <em>"Don't Solve Problems, Eliminate Them"</em> 
+                and you can <a href="${postHref}">read it online</a>.
+                <br>
+                How eliminating problems can drastically simplify your codebases and life
+              </p>
+            </div>
+            </content:encode>
+        </item>`;
+  }
 }
 
-function blogPostsRssXml(content) {
-  let latestPostDate = '';
-  let rssItemsXml = '';
+function getContent(locale) {
+  if (!locale) {
+    throw new Error('Missing locale');
+  }
 
-  content.forEach(
-    ({ publishedAt, urlPrefix, category, description, slug, title }) => {
-      const postDate = Date.parse(publishedAt);
-
-      //   // Remember to change this URL to your own!
-      const postHref = `${URL}/${urlPrefix}/${slug}`;
-
-      if (!latestPostDate || postDate > Date.parse(latestPostDate)) {
-        latestPostDate = publishedAt;
+  const query = `
+      query Content($locale: String) {
+        rss(locale: $locale) {
+          title
+          description
+        }
+        defaultSeo {
+          title
+          description
+        }
+        site {
+          url
+        }
+        tils(locale: $locale) {
+          slug
+          publishedAt
+          title
+          slug
+          description: title
+        }
+        posts(locale: $locale) {
+          slug
+          publishedAt: createdAt
+          title
+          slug
+          description
+        }
       }
+`;
 
-      rssItemsXml += `
-        <item>
-          <guid>${postHref}</guid>
-          <title><![CDATA[${title}]]></title>
-          <link>${postHref}</link>
-          <pubDate>${new Date(publishedAt).toUTCString()}</pubDate>
-          <guid isPermaLink="false">${postHref}</guid>
-          <category>${category}</category>
-          <description>
-          <![CDATA[${description}]]>
-          </description>
-          <content:encode>
-          <div style="width: 100%; margin: 0 auto; max-width: 800px; padding: 40px 40px;">
-            <p>
-              I've posted a new article <em>"Don't Solve Problems, Eliminate Them"</em> and you can <a href="https://kentcdodds.com/blog/don-t-solve-problems-eliminate-them">read it online</a>.
-              <br>
-              How eliminating problems can drastically simplify your codebases and life
+  return fetch(`${API_ENDPOINT}/graphql`, {
+    method: 'POST',
+    body: JSON.stringify({
+      query,
+      variables: {
+        locale,
+      },
+    }),
+    headers: { 'Content-Type': 'application/json' },
+  }).then(async (r) => {
+    const { data } = await r.json();
+    return data;
+  });
+}
 
-              <br>
-              You can also <a href="https://kentcdodds.com/subscribe">subscribe</a> for weekly emails on what I'm learning, working on, and writing about.
-            </p>
-          </div>
-          </content:encode>
-      </item>`;
-    },
-  );
-
-  return {
-    rssItemsXml,
-    latestPostDate: new Date(latestPostDate).toUTCString(),
-  };
+/**
+ *
+ * @param {string} date
+ * @returns
+ */
+function humanizeDate(date) {
+  return new Date(date).toUTCString();
 }
