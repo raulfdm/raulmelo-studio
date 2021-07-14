668ddd3f8c66ee961f498bede189d13b7d31d59b
Raul Melo <melo.raulf@gmail.com>
2021-07-14 14:46:01 +0200

    fix: url via env var

--- apps/website-svelte/src/config/app.ts
+++ apps/website-svelte/src/config/app.ts
@@ -1,3 +1,4 @@
-export const API_URL = import.meta.env.API_ENDPOINT || 'http://localhost:1337';
+export const API_URL =
+  import.meta.env.VITE_API_ENDPOINT ?? 'http://localhost:1337';
 
 export const IS_DEVELOPMENT = import.meta.env.DEV;
