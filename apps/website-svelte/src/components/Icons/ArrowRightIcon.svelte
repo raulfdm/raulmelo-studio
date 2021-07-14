a6bfee9a4d4adfac234611793b12937bd72b0700
Raul Melo <melo.raulf@gmail.com>
2021-07-14 17:40:00 +0200

    chore: add ArrowRightIcon

---
+++ apps/website-svelte/src/components/Icons/ArrowRightIcon.svelte
@@ -0,0 +1,16 @@
+<svg viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path
+    d="M5 16H27"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+  <path
+    d="M18 7L27 16L18 25"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+</svg>
