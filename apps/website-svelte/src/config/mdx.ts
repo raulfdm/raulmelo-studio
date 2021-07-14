b88f63fa4039949b3116f8736965767a841124d9
Raul Melo <melo.raulf@gmail.com>
2021-07-14 06:55:25 +0200

    chore: scaffold the project and load initial data for home

---
+++ apps/website-svelte/src/config/mdx.ts
@@ -0,0 +1,35 @@
+import { serialize } from 'next-mdx-remote/serialize';
+import rehypeAutoLink from 'rehype-autolink-headings';
+import rehypeSlug from 'rehype-slug';
+import mdxPrims, { MdxPrismOptions } from 'mdx-prism-2';
+import remarkCodeTitle from 'remark-code-titles';
+import remarkUnwrapImages from 'remark-unwrap-images';
+
+export function serializeMdx(source: string): ReturnType<typeof serialize> {
+  return serialize(source, {
+    mdxOptions: {
+      remarkPlugins: [remarkUnwrapImages, remarkCodeTitle] as never,
+      rehypePlugins: [
+        [
+          mdxPrims,
+          {
+            ignoreMissing: true,
+            lineHighlight: {
+              component: 'span',
+            },
+          } as MdxPrismOptions,
+        ],
+        rehypeSlug,
+        [
+          rehypeAutoLink,
+          {
+            properties: {
+              className: 'copy-title-icon',
+            },
+            content: null,
+          },
+        ],
+      ] as never,
+    },
+  });
+}
