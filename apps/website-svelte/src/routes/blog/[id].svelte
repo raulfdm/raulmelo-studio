9d8e4a62126f245ab4ab50b9cca21b35c429f6cb
Raul Melo <melo.raulf@gmail.com>
2021-07-16 20:24:28 +0200

    feat: attempt to add mdx

---
+++ apps/website-svelte/src/routes/blog/[id].svelte
@@ -0,0 +1,37 @@
+<script context="module">
+  export async function load() {
+    const mdsvex = await import('mdsvex/dist/browser-umd.js');
+
+    console.log(mdsvex);
+
+    const data = await mdsvex.compile(
+      `
+      # Hello World
+
+      - Foo
+      - bar
+    `,
+      {},
+    );
+
+    return {
+      props: {
+        data,
+      },
+    };
+  }
+</script>
+
+<script lang="ts">
+  export let data = {};
+</script>
+
+<main>
+  {@html data.code}
+</main>
+
+<style>
+  main {
+    grid-column: 1 / -1;
+  }
+</style>
