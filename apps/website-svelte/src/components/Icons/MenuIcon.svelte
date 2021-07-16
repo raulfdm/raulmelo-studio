28382bf6f0c42acfb40ef32b4df8194c6a41058f
Raul Melo <melo.raulf@gmail.com>
2021-07-16 10:42:27 +0200

    chore: add cross and menu icon

---
+++ apps/website-svelte/src/components/Icons/MenuIcon.svelte
@@ -0,0 +1,30 @@
+<svg viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path
+    d="M5 8.5H27"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+  <path
+    d="M5 13.5H27"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+  <path
+    d="M5.00073 18.5H27"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+  <path
+    d="M5.00073 23.5H27"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+</svg>
