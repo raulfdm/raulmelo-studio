bd62a91717f05b621beb88cb90103560790a910b
Raul Melo <melo.raulf@gmail.com>
2021-07-18 11:41:39 +0200

    fix(__layout): incorrect tagging

--- apps/website-svelte/src/routes/__layout.svelte
+++ apps/website-svelte/src/routes/__layout.svelte
@@ -10,34 +10,32 @@
   <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css" />
 </svelte:head>
 
-<MenuBar>
-  <main>
-    <slot />
-  </main>
+<MenuBar />
+<main>
+  <slot />
+</main>
 
-  <style>
+<style>
+  main {
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(4, minmax(0, 1fr));
+    gap: 1rem;
+    padding: 0 1rem;
+    margin: 0 auto;
+  }
+
+  @media screen and (min-width: 768px) {
     main {
-      width: 100%;
-      display: grid;
-      grid-template-columns: repeat(4, minmax(0, 1fr));
-      gap: 1rem;
-      padding: 0 1rem;
-      margin: 0 auto;
+      max-width: 64rem;
+      grid-template-columns: repeat(6, minmax(0, 1fr));
     }
+  }
 
-    @media screen and (min-width: 768px) {
-      main {
-        max-width: 64rem;
-        grid-template-columns: repeat(6, minmax(0, 1fr));
-      }
+  @media screen and (min-width: 1024px) {
+    main {
+      max-width: 80rem;
+      grid-template-columns: repeat(12, minmax(0, 1fr));
     }
-
-    @media screen and (min-width: 1024px) {
-      main {
-        max-width: 80rem;
-        grid-template-columns: repeat(12, minmax(0, 1fr));
-      }
-    }
-  </style>
-</MenuBar>
-<slot />
+  }
+</style>
