cfcd6b077aabfaf7deab2e450021dd3d67235145
Raul Melo <melo.raulf@gmail.com>
2021-07-18 09:03:27 +0200

    feat: add tailwind with svelte-add

--- apps/website-svelte/src/routes/__layout.svelte
+++ apps/website-svelte/src/routes/__layout.svelte
@@ -1,4 +1,5 @@
 <script>
+  import '../app.postcss';
   import MenuBar from '@components/MenuBar.svelte';
   import '../styles/globals.css';
   import '../styles/utilities.css';
@@ -9,32 +10,34 @@
   <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css" />
 </svelte:head>
 
-<MenuBar />
-<main>
-  <slot />
-</main>
+<MenuBar>
+  <main>
+    <slot />
+  </main>
 
-<style>
-  main {
-    width: 100%;
-    display: grid;
-    grid-template-columns: repeat(4, minmax(0, 1fr));
-    gap: 1rem;
-    padding: 0 1rem;
-    margin: 0 auto;
-  }
-
-  @media screen and (min-width: 768px) {
+  <style>
     main {
-      max-width: 64rem;
-      grid-template-columns: repeat(6, minmax(0, 1fr));
+      width: 100%;
+      display: grid;
+      grid-template-columns: repeat(4, minmax(0, 1fr));
+      gap: 1rem;
+      padding: 0 1rem;
+      margin: 0 auto;
     }
-  }
 
-  @media screen and (min-width: 1024px) {
-    main {
-      max-width: 80rem;
-      grid-template-columns: repeat(12, minmax(0, 1fr));
+    @media screen and (min-width: 768px) {
+      main {
+        max-width: 64rem;
+        grid-template-columns: repeat(6, minmax(0, 1fr));
+      }
     }
-  }
-</style>
+
+    @media screen and (min-width: 1024px) {
+      main {
+        max-width: 80rem;
+        grid-template-columns: repeat(12, minmax(0, 1fr));
+      }
+    }
+  </style>
+</MenuBar>
+<slot />
