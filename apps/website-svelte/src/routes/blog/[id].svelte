01bb9c87f61e8602dd1cdff2a9b130ee89dfc53e
Raul Melo <melo.raulf@gmail.com>
2021-07-19 08:15:20 +0200

    chore: poc

---
+++ apps/website-svelte/src/routes/blog/[id].svelte
@@ -0,0 +1,18 @@
+<script context="module" lang="ts">
+  export async function load({ fetch }) {
+    const response = await fetch(`/blog/${1}.json`);
+    const code = await response.json();
+
+    return {
+      props: {
+        code,
+      },
+    };
+  }
+</script>
+
+<script>
+  import Parser from './_Parser.svelte';
+</script>
+
+<Parser type="h1" raw="AHHH" class="foo" />
