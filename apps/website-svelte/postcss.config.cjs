01361188e3951984f031d06d8313f377c42e49d1
Raul Melo <melo.raulf@gmail.com>
2021-08-18 07:47:55 +0200

    refactor(website-svelte): replace local style setup with new package

--- apps/website-svelte/postcss.config.cjs
+++ apps/website-svelte/postcss.config.cjs
@@ -1,21 +1,3 @@
-const tailwindcss = require('tailwindcss');
-const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
+const { configuredConfig } = require('@raulmelo/styles/lib/postcss.config.cjs');
 
-const mode = process.env.NODE_ENV;
-const dev = mode === 'development';
-
-const config = {
-  plugins: [
-    //Some plugins, like postcss-nested, need to run before Tailwind,
-    tailwindcss(),
-    //But others, like autoprefixer, need to run after,
-    autoprefixer(),
-    !dev &&
-      cssnano({
-        preset: 'default',
-      }),
-  ],
-};
-
-module.exports = config;
+module.exports = configuredConfig;
