48fa58e63a28d4569f599879da325e803fe678fa
Raul Melo <melo.raulf@gmail.com>
2021-09-07 10:17:54 +0200

    fix: change fetchSiteData file extension

--- apps/website/scripts/build.sh
+++ apps/website/scripts/build.sh
@@ -4,6 +4,6 @@ echo "Build..."
 
 export API_ENDPOINT=https://raulmelo-dev-server.herokuapp.com
 
-node ./scripts/fetchSiteData.js
+node ./scripts/fetchSiteData.mjs
 yarn generateRss
 yarn next build
--- apps/website/scripts/fetchSiteData.mjs
+++ apps/website/scripts/fetchSiteData.mjs
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
 
-const fetch = require('node-fetch');
-const fs = require('fs');
-const path = require('path');
+import fetch from 'node-fetch';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const URL = process.env.API_ENDPOINT ?? 'http://localhost:1337';
 
