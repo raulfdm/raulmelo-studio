753e5318b5b50dc1d489c4e91ae34eacb37b43d4
Raul Melo <melo.raulf@gmail.com>
2021-09-18 15:46:32 +0200

    chore: clean up on fetcher function

--- packages/core/src/utils/fetcher.ts
+++ packages/core/src/utils/fetcher.ts
@@ -2,41 +2,36 @@ import 'isomorphic-fetch';
 import { API_URL } from '~config';
 import { utils } from '~utils';
 
-async function internalFetcher(url: string, opts?: RequestInit) {
-  const res = await fetch(url, opts);
-
-  return await res.json();
-}
-
 type Variables = {
   [key: string]: string | boolean | number | null | Variables;
 };
 
 export const fetcher = {
-  // TODO: this should be elsewhere
-  // graphqlVariables: {
-  //   preview: {
-  //     _publicationState: 'preview',
-  //     published_at_null: true,
-  //   },
-  // },
   async graphql<T>(query: string, variables?: Variables): Promise<T> {
     const url = `${API_URL}/graphql`;
 
-    const res = await internalFetcher(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
-      body: JSON.stringify({ query, variables }),
-    });
+    try {
+      const res = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+        body: JSON.stringify({ query, variables }),
+      });
 
-    if (!utils.isNil(res.errors)) {
-      console.log('ERRORS ->', res.errors);
-      throw new Error('Backend.graphql: Something went wrong. Check console');
+      const data = await res.json();
+
+      if (!utils.isNil(data.errors)) {
+        throw new Error(data.errors);
+      }
+
+      return data;
+    } catch (error) {
+      console.error(
+        'Backend.graphql: Something went wrong. Check console',
+        error,
+      );
     }
-
-    return res.data;
   },
 };
