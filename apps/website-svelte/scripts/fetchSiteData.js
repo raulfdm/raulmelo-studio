8cfe627742cda25fe0dab2cfdbe14552c00e71df
Raul Melo <melo.raulf@gmail.com>
2021-09-18 16:44:00 +0200

    refactor: replace custom script with script from core

--- apps/website-svelte/scripts/fetchSiteData.js
+++ apps/website-svelte/scripts/fetchSiteData.js
@@ -5,90 +5,13 @@
  * a ESM context. That means I don't have access to `require`,
  * `__dirname`, etc.
  */
-import fetch from 'node-fetch';
-import fs from 'fs';
+import { generateSiteData } from '@raulfdm/core/generateSiteData';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-const URL = process.env.VITE_API_ENDPOINT ?? 'http://localhost:1337';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-if (URL.includes('localhost')) {
-  console.log('Getting data from Localhost');
-}
-
-const query = `
-query SiteData {
-  defaultSeoPt: defaultSeo(locale: "pt") {
-    title
-    locale
-    description
-  }
-  defaultSeoEn: defaultSeo(locale: "en") {
-    title
-    locale
-    description
-  }
-  personalInformation {
-    full_name
-    profile_pic {
-      formats      
-    }
-  }
-  site {
-    url
-    seo_image {
-      url
-      width
-      height
-    }
-  }
-  socials {
-    url
-    username
-    name
-  }
-}
-`;
-
-const config = {
-  apiUrl: `${URL}/graphql`,
-  get distPath() {
-    const __filename = fileURLToPath(import.meta.url);
-    const __dirname = path.dirname(__filename);
-    return path.resolve(__dirname, '..', 'site-data.json');
-  },
-};
-
-fetch(config.apiUrl, {
-  method: 'POST',
-  body: JSON.stringify({ query }),
-  headers: { 'Content-Type': 'application/json' },
-})
-  .then((res) => res.json())
-  .then(async ({ data }) => {
-    const { defaultSeoPt, defaultSeoEn, personalInformation, ...rest } = data;
-    const { profile_pic, ...restPersonalInfo } = personalInformation;
-
-    /**
-     * Ensure of having both default seo locales
-     */
-    const sanitizedData = {
-      ...rest,
-      personalInformation: {
-        profile_pic: profile_pic.formats.thumbnail,
-        ...restPersonalInfo,
-      },
-      defaultSeo: {
-        pt: defaultSeoPt,
-        en: defaultSeoEn,
-      },
-    };
-
-    fs.writeFileSync(config.distPath, JSON.stringify(sanitizedData, null, 2));
-  })
-  .then(() => console.log('Site data generated at ', config.distPath))
-  .catch((err) => {
-    console.error('Something went wrong while generating site-data');
-    console.error(err);
-    process.exit(1);
-  });
+generateSiteData({
+  apiEndpoint: process.env.VITE_API_ENDPOINT,
+  outdir: path.resolve(__dirname, '..'),
+}).catch(console.error);
