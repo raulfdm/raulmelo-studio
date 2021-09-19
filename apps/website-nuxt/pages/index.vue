4bdf31e565a374e22c8f80b2392f18bcf3110813
Raul Melo <melo.raulf@gmail.com>
2021-09-19 06:30:45 +0200

    feat: add home page

--- apps/website-nuxt/pages/index.vue
+++ apps/website-nuxt/pages/index.vue
@@ -1,11 +1,25 @@
-<template>
-  <h1>hi :)</h1>
-</template>
-
 <script lang="ts">
-import { defineComponent } from '@nuxtjs/composition-api';
+import { domains } from '@raulfdm/core';
+import { defineComponent, useAsync } from '@nuxtjs/composition-api';
 
 export default defineComponent({
-  asyncData() {},
+  setup() {
+    const content = useAsync(() => domains.posts.queryPostsAndTils('en', 2));
+
+    return {
+      posts: content?.value?.posts,
+      tils: content?.value?.tils,
+    };
+  },
 });
 </script>
+
+<template>
+  <div>
+    <h1>hi :)</h1>
+    <ul>
+      <li v-for="post in posts" :key="post.id">{{ post.title }}</li>
+      <li v-for="til in tils" :key="til.id">{{ til.title }}</li>
+    </ul>
+  </div>
+</template>
