27de0b71215d455ef0b9787594de38dd7ec13c0b
Raul Melo <melo.raulf@gmail.com>
2021-09-19 06:26:52 +0200

    chore: converte nuxt config to ts

--- apps/website-nuxt/nuxt.config.ts
+++ apps/website-nuxt/nuxt.config.ts
@@ -11,17 +11,31 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'preconnect',
+        href: 'https://fonts.gstatic.com',
+        hid: 'font-preconnect',
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://use.typekit.net/rsd6fwc.css',
+        hid: 'typekit-fonts',
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['@raulmelo/styles/lib/all.css', '~/styles/body.postcss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+  components: {
+    dirs: ['~/components', '~/components/app'],
+  },
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
