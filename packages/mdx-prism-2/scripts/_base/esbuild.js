44f5183034510d7b347f6ffc69988b7117c9bef3
Raul Melo <melo.raulf@gmail.com>
2021-10-01 08:10:40 +0200

    refactor: remove external dependencies from the bundle
    BREAKING CHANGE: Now, we no longer add all dependencies in the final bundle.

--- packages/mdx-prism-2/scripts/_base/esbuild.js
+++ packages/mdx-prism-2/scripts/_base/esbuild.js
@@ -1,5 +1,6 @@
 import { dtsPlugin } from 'esbuild-plugin-d.ts';
-import { build } from 'esbuild';
+import { build, analyzeMetafile } from 'esbuild';
+import pkgJson from '../../package.json';
 
 export function runESBuild(options = {}) {
   const commonJs = createConfig({
@@ -15,13 +16,26 @@ export function runESBuild(options = {}) {
     target: 'node12',
   });
 
-  return Promise.all([build(commonJs), build(esm)]);
+  return Promise.all([build(commonJs), build(esm)]).then(
+    async ([cjsOutput, esmOutput]) => {
+      const esmResult = await analyzeMetafile(esmOutput.metafile, {
+        verbose: false,
+      });
+      console.log(esmResult);
+      const cjsResult = await analyzeMetafile(cjsOutput.metafile, {
+        verbose: false,
+      });
+      console.log(cjsResult);
+    },
+  );
 
   function createConfig(overrides = {}) {
     const baseConfig = {
       bundle: true,
       entryPoints: ['src/lib.ts'],
       sourcemap: 'external',
+      metafile: true,
+      external: Object.keys(pkgJson.dependencies),
       ...overrides,
     };
 
