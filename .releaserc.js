05bfbc18cbf77eeaa3a8cd6bc5538489f17433aa
Raul Melo <melo.raulf@gmail.com>
2021-10-22 12:36:29 +0200

    chore: remove lerna

--- .releaserc.js
+++ .releaserc.js
@@ -1,25 +1,25 @@
-module.exports = {
-  extends: 'semantic-release-monorepo',
-  branches: ['main'],
-  plugins: [
-    [
-      '@semantic-release/commit-analyzer',
-      {
-        releaseRules: [
-          { type: 'chore', release: 'patch' },
-          { type: 'chore', scope: 'deps-dev', release: false },
-          { type: 'refactor', release: 'major' },
-        ],
-      },
-    ],
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/npm',
-    '@semantic-release/git',
-    [
-      '@semantic-release/github',
-      {
-        assets: ['.'],
-      },
-    ],
-  ],
-};
+// module.exports = {
+//   extends: 'semantic-release-monorepo',
+//   branches: ['main'],
+//   plugins: [
+//     [
+//       '@semantic-release/commit-analyzer',
+//       {
+//         releaseRules: [
+//           { type: 'chore', release: 'patch' },
+//           { type: 'chore', scope: 'deps-dev', release: false },
+//           { type: 'refactor', release: 'major' },
+//         ],
+//       },
+//     ],
+//     '@semantic-release/release-notes-generator',
+//     '@semantic-release/npm',
+//     '@semantic-release/git',
+//     [
+//       '@semantic-release/github',
+//       {
+//         assets: ['.'],
+//       },
+//     ],
+//   ],
+// };
