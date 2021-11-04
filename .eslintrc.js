46cc1946868427ca4cda1f7f84df9a455aa4962d
Raul Melo <melo.raulf@gmail.com>
2021-11-04 07:41:49 +0100

    chore: fix eslint parser options

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,12 @@ const baseTs = {
 
 const baseJs = {
   parser: '@babel/eslint-parser',
+  /**
+   * https://github.com/babel/ember-cli-babel/issues/366#issuecomment-728947041
+   */
+  parserOptions: {
+    requireConfigFile: false,
+  },
   extends: ['eslint:recommended'],
 };
 
