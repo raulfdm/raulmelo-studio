deee790d0e589289afd4bc6b887be933726f2978
Raul Melo <melo.raulf@gmail.com>
2021-11-13 06:40:39 +0100

    chore(ci): set back rebase when conflicted

--- config/renovate-config.js
+++ config/renovate-config.js
@@ -11,7 +11,7 @@ module.exports = {
   /**
    * Renovate options
    */
-  rebaseWhen: 'behind-base-branch',
+  rebaseWhen: 'conflicted',
   branchPrefix: 'renovate/',
   gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
   includeForks: true,
