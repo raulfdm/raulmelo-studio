2ead65ad8957c087e79402d944245e05527ce476
Raul Melo <melo.raulf@gmail.com>
2022-01-30 12:16:55 +0100

    chore: add css modules preset

--- packages/ui/.storybook/main.js
+++ packages/ui/.storybook/main.js
@@ -1,8 +1,9 @@
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
+    'storybook-css-modules-preset',
     {
       name: '@storybook/addon-postcss',
       options: {
