286e41aa36288915dde62607d6b96b76487c4ddb
Raul Melo <melo.raulf@gmail.com>
2022-01-30 11:04:17 +0100

    chore: tweak HEADING styles

--- packages/ui/src/components/Headings/Headings.tsx
+++ packages/ui/src/components/Headings/Headings.tsx
@@ -1,49 +1,8 @@
-import tw, { css, styled } from 'twin.macro';
+import styles from './Heading.module.css';
 
-const common = css`
-  ${tw`relative table`};
-
-  --icon-visible: hidden;
-
-  &:hover {
-    --icon-visible: unset;
-  }
-
-  .copy-title-icon {
-    ${tw`absolute -right-1`};
-    transform: translate(100%, 50%);
-
-    .icon-link {
-      ${tw`block bg-secondary`};
-      width: 0.7em;
-      height: 0.7em;
-      mask: url(/icons/anchor.svg) no-repeat;
-      transition: visibility 0.2s ease-in-out;
-      visibility: var(--icon-visible);
-    }
-  }
-`;
-
-export const H1 = styled.h1`
-  ${common};
-`;
-
-export const H2 = styled.h2`
-  ${common};
-`;
-
-export const H3 = styled.h3`
-  ${common};
-`;
-
-export const H4 = styled.h4`
-  ${common};
-`;
-
-export const H5 = styled.h5`
-  ${common};
-`;
-
-export const H6 = styled.h6`
-  ${common};
-`;
+export const H1 = (props) => <h1 {...props} className={styles.heading} />;
+export const H2 = (props) => <h2 {...props} className={styles.heading} />;
+export const H3 = (props) => <h3 {...props} className={styles.heading} />;
+export const H4 = (props) => <h4 {...props} className={styles.heading} />;
+export const H5 = (props) => <h5 {...props} className={styles.heading} />;
+export const H6 = (props) => <h6 {...props} className={styles.heading} />;
