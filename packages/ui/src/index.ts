03094dee53be1dba0666b69fa988d04906662721
Raul Melo <melo.raulf@gmail.com>
2022-01-30 19:43:31 +0100

    chore: remove incorrect import

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -3,7 +3,6 @@ export * from './components/CodePenIframe';
 export * from './components/DotDivider';
 export * from './components/Gif';
 export * from './components/Headings';
-export * from './components/Image';
 export * from './components/ImageSlider';
 export * from './components/Tweet';
 export * from './components/YouTubeIframe';
