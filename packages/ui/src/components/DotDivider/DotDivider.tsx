a97c2b38ee6b2628048c16fc3feed1b3ad3aaae2
Raul Melo <melo.raulf@gmail.com>
2022-01-30 10:55:42 +0100

    chore: tweak dot divider

--- packages/ui/src/components/DotDivider/DotDivider.tsx
+++ packages/ui/src/components/DotDivider/DotDivider.tsx
@@ -1,30 +1,6 @@
-// import styled from '@emotion/styled';
-import React from 'react';
-import tw, { css, styled } from 'twin.macro';
+import styles from './DotDivider.module.css';
 
-const Dot = styled.div(() => [
-  tw`w-1.5 h-1.5 lg:width[0.475rem] lg:height[0.475rem]`,
-  tw`rounded-full`,
-  css`
-    --hr-color: var(--color-secondary);
-    background-color: var(--hr-color);
-
-    /* IE and Legacy Edge */
-    @media screen and (-ms-high-contrast: active) {
-      --hr-color: windowText;
-    }
-    /* Using the new standards for forced colors, currently supported in Chromium Edge */
-    @media screen and (forced-colors: active) {
-      --hr-color: CanvasText;
-    }
-
-    /* For all variants, we need this: */
-    & + * {
-      margin-top: 0;
-    }
-  `,
-]);
-
+const Dot = () => <div aria-hidden="true" className={styles.dot} />;
 /**
  * I'm currently use 3 sizes of tailwind typography:
  * - lg => lower than md:
@@ -37,17 +13,10 @@ const Dot = styled.div(() => [
 
 export const DotDivider = () => {
   return (
-    <div
-      role="separator"
-      data-testid="dot-divider"
-      css={[
-        tw`flex justify-center space-x-3`,
-        tw`margin[3.111em 0px] md:margin[2.8em 0px] lg:margin[3em 0px]`,
-      ]}
-    >
-      <Dot aria-hidden />
-      <Dot aria-hidden />
-      <Dot aria-hidden />
+    <div role="separator" data-testid="dot-divider" className={styles.divider}>
+      <Dot />
+      <Dot />
+      <Dot />
     </div>
   );
 };
