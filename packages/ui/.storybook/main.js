5ec87262f11954cbcca1509829d4c46fc84f37cf
Raul Melo <melo.raulf@gmail.com>
2022-01-30 11:20:10 +0100

    feat: add base storybook

---
+++ packages/ui/.storybook/main.js
@@ -0,0 +1,16 @@
+module.exports = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('../../../node_modules/postcss'),
+        },
+      },
+    },
+  ],
+  framework: '@storybook/react',
+};
