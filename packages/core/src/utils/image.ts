c3626d2abf0ec934da30873a2c3831390d640c3b
Raul Melo <melo.raulf@gmail.com>
2022-02-05 11:23:39 +0100

    feat: add utility to get sanity image size

--- packages/core/src/utils/image.ts
+++ packages/core/src/utils/image.ts
@@ -2,10 +2,35 @@ import imageUrlBuilder from '@sanity/image-url';
 
 import { sanityConfig } from '~config';
 
+import { isNil } from './ramda';
+
 const builder = imageUrlBuilder(sanityConfig);
 
 export function imgUrlFor(source: SanityImageSource) {
   return builder.image(source);
+}
+
+// TODO: unit test it
+export function getImageDimensionsFromSanityImageUrl(url: string) {
+  /**
+   * Sanity image URLs look like this:
+   * "http://some-url/aaksdjaksdj-245x153.some-extension"
+   *                              ^^^^^^^
+   *
+   * This utility extracts this information since the builder image utility
+   * from sanity does not provide the dimensions
+   */
+  const REGEX = /-(\d*)x(\d*)./gm;
+  const [, width, height] = REGEX.exec(url);
+
+  if (isNil(width) || isNil(height)) {
+    throw new Error(`Could not parse image dimensions from url => "${url}"`);
+  }
+
+  return {
+    width: parseInt(width, 10),
+    height: parseInt(height, 10),
+  };
 }
 
 export type SanityImageSource =
