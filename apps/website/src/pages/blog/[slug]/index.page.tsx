2c42894995147434263cef99143ccb5243516c02
Raul Melo <melo.raulf@gmail.com>
2022-02-06 09:57:19 +0100

    chore: revert blog page changes

--- apps/website/src/pages/blog/[slug]/index.page.tsx
+++ apps/website/src/pages/blog/[slug]/index.page.tsx
@@ -1,15 +1,12 @@
-import { PortableText } from '@portabletext/react';
 import { domains, utils } from '@raulmelo/core';
-import { BigQuote, DotDivider, ProseContainer } from '@raulmelo/ui';
+import { DotDivider } from '@raulmelo/ui';
 import fs from 'fs';
 import { GetStaticPaths } from 'next';
 import dynamic from 'next/dynamic';
 import path from 'path';
 import React from 'react';
 
-import { PrismStyles } from '~/components/MdxPost/components/PrismStyles';
-import { PortableTextPost } from '~/components/PortableTextPost/PortableTextPost';
-// import { MdxPostTemplate } from '~/components/MdxPost';
+import { MdxPostTemplate } from '~/components/MdxPost';
 import { serializeMdx } from '~/config/mdx';
 
 import type { SeriesSection as SeriesSectionType } from './components/SeriesSection';
@@ -23,7 +20,6 @@ export const BlogPostPage: React.FC<BlogPostProps> = ({
   content,
   post,
   preview,
-  sanityParsedContent,
 }) => {
   const { featured_image, post_tags, unsplash, series } = post;
 
@@ -39,8 +35,8 @@ export const BlogPostPage: React.FC<BlogPostProps> = ({
   ) : null;
 
   return (
-    <PortableTextPost
-      content={JSON.parse(sanityParsedContent).body}
+    <MdxPostTemplate
+      content={content}
       postContent={post.content}
       preview={preview}
       featuredImage={{
@@ -62,30 +58,6 @@ export const BlogPostPage: React.FC<BlogPostProps> = ({
         bottom: seriesWithDivider,
       }}
     />
-
-    // <MdxPostTemplate
-    // content={content}
-    // postContent={post.content}
-    // preview={preview}
-    // featuredImage={{
-    //   src: featured_image.url,
-    //   unsplash,
-    //   width: featured_image.width,
-    //   height: featured_image.height,
-    // }}
-    // title={post.title}
-    // subtitle={post.subtitle}
-    // publishedAt={post.date}
-    // // share={{
-    // //   description: `${post.title}. ${post.subtitle}`,
-    // // }}
-    // tags={post_tags}
-    // description={post.description}
-    // series={{
-    //   top: allSeries,
-    //   bottom: seriesWithDivider,
-    // }}
-    // />
   );
 };
 
