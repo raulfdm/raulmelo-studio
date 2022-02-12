02a449f12cb6cf319ebd541d58902ad81000208c
Raul Melo <melo.raulf@gmail.com>
2022-02-12 18:43:21 +0100

    chore: disable CDN

--- packages/core/src/config/index.ts
+++ packages/core/src/config/index.ts
@@ -4,7 +4,7 @@ export const sanityConfig = {
   projectId: 'gc3hakk3',
   dataset: 'production',
   apiVersion: 'v1',
-  useCdn: true, // `false` if you want to ensure fresh data
+  useCdn: false, // `false` if you want to ensure fresh data
 };
 
 export const client = sanityClient(sanityConfig);
