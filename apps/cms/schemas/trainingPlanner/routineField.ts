fec1e7bcda6fc1ade05f69ab7ba704e52d5853e0
Raul Melo <melo.raulf@gmail.com>
2022-04-01 08:07:42 +0200

    Add my fitness buddy app (#1472)
    * feat: add application
    
    * chore: reestructure deskStructure to support multiple types
    
    * chore: move blog post schemas to blog folder
    
    * chore: start creating exercise model
    
    * feat: finish training data

---
+++ apps/cms/schemas/trainingPlanner/routineField.ts
@@ -0,0 +1,103 @@
+export const trainingRoutineField = {
+  name: 'trainingRoutine',
+  title: 'Training Routine',
+  type: 'object',
+  fields: [
+    {
+      type: 'object',
+      name: 'routine',
+      fields: [
+        {
+          name: 'name',
+          type: 'string',
+        },
+        {
+          name: 'description',
+          type: 'string',
+        },
+        {
+          name: 'date',
+          type: 'date',
+        },
+        {
+          name: 'training',
+          title: 'Training',
+          type: 'array',
+          of: [
+            {
+              type: 'object',
+              name: 'training',
+              title: 'Training',
+              fields: [
+                {
+                  name: 'exercise',
+                  type: 'reference',
+                  to: { type: 'exercise' },
+                },
+                { name: 'series', title: 'Series', type: 'number' },
+                {
+                  name: 'repetitions',
+                  title: 'Repetitions',
+                  type: 'string',
+                },
+                { name: 'restTime', title: 'Rest Time', type: 'number' },
+                {
+                  title: 'Advanced Technique',
+                  name: 'advancedTechnique',
+                  type: 'string',
+                  options: {
+                    list: [
+                      { title: `BI-Set`, value: 'bi_set' },
+                      { title: `FST-7`, value: 'fst_7' },
+                      { title: `GVT`, value: 'gvt' },
+                      {
+                        title: `Rest 'n' Pause 3x`,
+                        value: 'rest_and_pause',
+                      },
+                    ],
+                  },
+                },
+              ],
+              preview: {
+                select: {
+                  exercise: 'exercise.name',
+                  series: 'series',
+                  repetitions: 'repetitions',
+                },
+                prepare(selection) {
+                  return {
+                    title: selection.exercise,
+                    subtitle: `${selection.series}x${selection.repetitions}`,
+                  };
+                },
+              },
+            },
+          ],
+        },
+        {
+          name: 'cardio',
+          type: 'object',
+          fields: [
+            {
+              name: 'time',
+              title: 'Time',
+              description: 'Time in minutes',
+              type: 'number',
+            },
+          ],
+        },
+      ],
+    },
+  ],
+  preview: {
+    select: {
+      routine: 'routine',
+    },
+    prepare(selection) {
+      return {
+        title: selection.routine.name,
+        subtitle: selection.routine.description,
+      };
+    },
+  },
+};
