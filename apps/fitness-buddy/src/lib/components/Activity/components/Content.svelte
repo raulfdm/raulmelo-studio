1ca9ef1d69b0cabfce678f700836c28d55627f10
Raul Melo <melo.raulf@gmail.com>
2022-04-07 06:54:40 +0200

    feat: implemnet activity content

---
+++ apps/fitness-buddy/src/lib/components/Activity/components/Content.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  import ContentTitle from '$lib/components/ContentTitle.svelte';
+
+  import { activityStore } from '../activityStore';
+  console.log($activityStore.currentTraining);
+  const youtubeUrl = $activityStore.currentTraining.exercise.youtubeVideoId
+    ? `https://www.youtube.com/embed/${$activityStore.currentTraining.exercise.youtubeVideoId}`
+    : undefined;
+</script>
+
+<ContentTitle title={$activityStore.currentTraining.exercise.name} />
+
+{#if youtubeUrl}
+  <div class="aspect-w-16 aspect-h-9">
+    <iframe
+      class="w-full "
+      src={youtubeUrl}
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    />
+  </div>
+{:else if $activityStore.currentTraining.exercise.image.url}
+  <figure class="aspect-w-16 aspect-h-9">
+    <img
+      class="object-cover object-center w-full h-full"
+      src={$activityStore.currentTraining.exercise.image.url}
+      alt={$activityStore.currentTraining.exercise.name}
+    />
+  </figure>
+{:else}
+  <ContentTitle title="Nenhuma imagem" />
+{/if}
+
+<style lang="postcss">
+  .title {
+  }
+</style>
