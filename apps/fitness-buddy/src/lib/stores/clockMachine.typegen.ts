9fd9fbaba2d7af860e4cc40e5beeaa2c1dfc302d
Raul Melo <melo.raulf@gmail.com>
2022-04-16 16:16:19 +0200

    chore: add typegen to clock machine

---
+++ apps/fitness-buddy/src/lib/stores/clockMachine.typegen.ts
@@ -0,0 +1,32 @@
+// This file was automatically generated. Edits will be overwritten
+
+export interface Typegen0 {
+  '@@xstate/typegen': true;
+  eventsCausingActions: {
+    setActivity: 'SET_ACTIVITY';
+    startClock: 'TOGGLE';
+    fastForward: 'FAST_FORWARD';
+    rewind: 'REWIND';
+    pauseClock: 'TOGGLE';
+    tick: 'TICK';
+    startNextSession: 'xstate.init';
+  };
+  internalEvents: {
+    'xstate.init': { type: 'xstate.init' };
+  };
+  invokeSrcNameMap: {};
+  missingImplementations: {
+    actions: never;
+    services: never;
+    guards: never;
+    delays: never;
+  };
+  eventsCausingServices: {};
+  eventsCausingGuards: {
+    canFastForward: 'FAST_FORWARD';
+    canRewind: 'REWIND';
+  };
+  eventsCausingDelays: {};
+  matchesStates: 'unset' | 'idle' | 'running' | 'pause';
+  tags: never;
+}
