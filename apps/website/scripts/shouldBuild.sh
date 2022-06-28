d2456110affeb419fb6ed671795b3aaa86055656
Raul Melo <melo.raulf@gmail.com>
2022-06-28 08:28:35 +0200

    fix: shouldBuild script

--- apps/website/scripts/shouldBuild.sh
+++ apps/website/scripts/shouldBuild.sh
@@ -5,6 +5,6 @@
 # https://vercel.com/support/articles/how-do-i-use-the-ignored-build-step-field-on-vercel
 
 # This command exits with 0 if nothing has changed or 1 if something has changed in:
-#                           \/-- apps/website (because of the "root" folder I've defined there)
-git diff HEAD^ HEAD --quiet ./ ../../packages
-#                               /\--- packages this project depends on
+#                           \/-- Application
+git diff HEAD^ HEAD --quiet ./apps/website ./packages/
+#                                          /\--- packages this project depends on
