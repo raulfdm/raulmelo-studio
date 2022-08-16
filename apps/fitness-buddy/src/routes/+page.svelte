a7ad346baae660c8b6436963ddd6b917b7ca8578
Raul Melo <melo.raulf@gmail.com>
2022-08-16 09:21:38 +0200

    svelte-migrate: renamed files

--- apps/fitness-buddy/src/routes/+page.svelte
+++ apps/fitness-buddy/src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  throw new Error("@migration task: Add data prop (https://github.com/sveltejs/kit/discussions/5774#discussioncomment-3292707)");
+
   import { onMount } from 'svelte';
   import type { ITrainingSheet } from '$lib/api';
   import LoaderSpinner from '$lib/components/LoaderSpinner.svelte';
