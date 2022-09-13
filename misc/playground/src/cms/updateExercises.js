d881246b1f9bb3001ee15c90e5a6d8da39925629
Raul de Melo <melo.raulf@gmail.com>
2022-09-13 17:45:26 +0200

    chore: improve update exercises script

--- misc/playground/src/cms/updateExercises.js
+++ misc/playground/src/cms/updateExercises.js
@@ -28,37 +28,66 @@ const query = groq`
   }
 }
 `;
+let counter = 0;
+const fails = [];
 
 try {
   const exercises = await raulClient.fetch(query);
+  console.log('Number of exercises:', exercises.length);
+
+  exercises.forEach((exercise) => {
+    if (exercise.image === undefined) {
+      console.log(exercise._id);
+    }
+  });
+
   const transactions = camilaClient.transaction();
 
+  console.log('Start the migration');
   for await (const exercise of exercises) {
+    console.group();
     const { _id, _type, name, video, image } = exercise;
-    if (!image?.url) {
-      break;
+
+    console.log(`${++counter}. ${name}`);
+
+    try {
+      const imageStream = got.stream(image.url);
+      console.log('Uploading image');
+      const sanityImage = await camilaClient.assets.upload(
+        'image',
+        imageStream,
+      );
+      console.log('Image uploaded');
+
+      transactions.createOrReplace({
+        _id,
+        _type,
+        name,
+        video,
+        image: {
+          _type: 'image',
+          asset: {
+            _ref: sanityImage._id,
+            _type: 'reference',
+          },
+        },
+      });
+      console.log('Item added\n--------');
+    } catch (error) {
+      console.log('FAIL');
+      fails.push({ _id, error, exercise });
     }
 
-    const imageStream = got.stream(image.url);
-    console.log('Uploading image for', name);
-    const sanityImage = await camilaClient.assets.upload('image', imageStream);
-
-    transactions.createOrReplace({
-      _id,
-      _type,
-      name,
-      video,
-      image: {
-        _type: 'image',
-        asset: {
-          _ref: sanityImage._id,
-          _type: 'reference',
-        },
-      },
-    });
+    console.groupEnd();
   }
 
+  console.log('Committing the transaction');
   await transactions.commit();
+  console.log('Migration completed');
 } catch (error) {
   console.log(error);
+}
+
+if (fails.length > 0) {
+  console.log(fails);
 }
