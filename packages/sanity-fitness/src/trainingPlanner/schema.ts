2a1b23b23673e608af6f21ba1e90d697054178f0
Raul de Melo <melo.raulf@gmail.com>
2022-09-13 07:33:55 +0200

    feat: initialize common sanity package

---
+++ packages/sanity-fitness/src/trainingPlanner/schema.ts
@@ -0,0 +1,34 @@
+export const trainingSchema = {
+  name: 'trainingSchema',
+  title: 'Training Schema',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Titulo',
+      type: 'string',
+    },
+    {
+      name: 'startDate',
+      title: 'Início',
+      type: 'date',
+    },
+    {
+      name: 'endDate',
+      title: 'Fim',
+      type: 'date',
+    },
+    {
+      name: 'schema',
+      title: 'Planilha',
+      type: 'array',
+      of: [
+        {
+          name: 'routine',
+          type: 'reference',
+          to: { type: 'trainingRoutine' },
+        },
+      ],
+    },
+  ],
+};
