9f29b8afb95a8b869f70d911c9aaf4e2f81fc148
Raul de Melo <melo.raulf@gmail.com>
2022-09-18 12:41:52 +0200

    chore: enhance styles

--- apps/utilities/src/routes/+page.svelte
+++ apps/utilities/src/routes/+page.svelte
@@ -1,10 +1,30 @@
-<nav class="flex gap-4">
-  <a href="/tools/intermittent-fasting">Intermitent Fasting</a>
-  <a href="/tools/rule-of-three">Rule of Three</a>
-</nav>
+<h1 class="mb-4 text-3xl italic font-bold text-center">
+  Damn Useful Utilities
+</h1>
+
+<ul class="text-xl text-gray-700">
+  <li>
+    Intermitent Fasting
+    <ul class="list-inside">
+      <li>
+        <a href="/tools/intermittent-fasting/backwards">Backwards</a>
+      </li>
+      <li>
+        <a href="/tools/intermittent-fasting/forward">Forward</a>
+      </li>
+    </ul>
+  </li>
+  <li>
+    <a href="/tools/rule-of-three">Rule of Three</a>
+  </li>
+</ul>
 
 <style>
   a {
-    @apply shadow p-4 block;
+    @apply underline;
+  }
+  ul {
+    @apply space-y-2;
+    list-style-type: revert;
   }
 </style>
---
+++ apps/utilities/src/routes/tools/intermittent-fasting/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import PageTitle from '$lib/components/PageTitle.svelte';
+</script>
+
+<PageTitle>Intermittent Fasting</PageTitle>
+
+<slot />
