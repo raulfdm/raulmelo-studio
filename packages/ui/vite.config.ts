faf7bb49cfbe10af24e17b94c8e0b0a89460c191
Raul de Melo <melo.raulf@gmail.com>
2022-11-19 16:53:55 +0100

    chore: temp include refractor into the lib bundle

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,7 +1,20 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 
-const isExternal = (id: string) => !id.startsWith('.') && !path.isAbsolute(id);
+function isExternal(id: string) {
+  /**
+   * TODO: I'm bundling the entire refractor library here because Remix
+   * is complaining about "requiring" an ESM dependency.
+   *
+   * I'm not sure how to fix that properly. Probably I'm going to open an issue
+   * on Remix and see what they say.
+   */
+  if (id.startsWith('refractor/')) {
+    return false;
+  }
+
+  return !id.startsWith('.') && !path.isAbsolute(id);
+}
 
 export default defineConfig(() => ({
   esbuild: {
