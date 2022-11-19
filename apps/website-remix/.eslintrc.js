1f1d63d0dea921652117adf36d8fbb2840767dc6
Raul de Melo <melo.raulf@gmail.com>
2022-11-19 11:02:26 +0100

    add eslint rules

--- apps/website-remix/.eslintrc.js
+++ apps/website-remix/.eslintrc.js
@@ -1,4 +1,24 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node"],
+  root: true,
+  extends: ['@remix-run/eslint-config', '@remix-run/eslint-config/node'],
+  rules: {
+    '@typescript-eslint/consistent-type-assertions': 'error',
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      { disallowTypeAnnotations: false },
+    ],
+  },
+  overrides: [
+    {
+      files: ['*.test.*'],
+      rules: {
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        'jsx-a11y/anchor-is-valid': 'off',
+        'jsx-a11y/alt-text': 'off',
+        'react/no-unescaped-entities': 'off',
+      },
+    },
+  ],
 };
