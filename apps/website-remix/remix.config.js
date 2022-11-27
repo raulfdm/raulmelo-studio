29ac2a44ee4aa840e5fba1c65b760e2bde80754c
Raul de Melo <melo.raulf@gmail.com>
2022-11-27 17:32:26 +0100

    chore: remove lib to be bundled up and change ramda imports

--- apps/website-remix/remix.config.js
+++ apps/website-remix/remix.config.js
@@ -6,9 +6,5 @@ module.exports = {
   // so we default back to the standard build output.
   server: process.env.NODE_ENV === 'development' ? undefined : './server.js',
   ignoredRouteFiles: ['**/.*'],
-  serverDependenciesToBundle: [
-    '@raulmelo/core',
-    '@raulmelo/sanity-core',
-    '@raulmelo/sanity-fitness',
-  ],
+  serverDependenciesToBundle: [],
 };
--- packages/core/src/utils/ramda.ts
+++ packages/core/src/utils/ramda.ts
@@ -1,7 +1,7 @@
-import head from 'ramda/src/head.js';
-import isEmpty from 'ramda/src/isEmpty.js';
-import isNil from 'ramda/src/isNil.js';
-import not from 'ramda/src/not.js';
-import omit from 'ramda/src/omit.js';
+import head from 'ramda/src/head';
+import isEmpty from 'ramda/src/isEmpty';
+import isNil from 'ramda/src/isNil';
+import not from 'ramda/src/not';
+import omit from 'ramda/src/omit';
 
 export { head, isEmpty, isNil, not, omit };
