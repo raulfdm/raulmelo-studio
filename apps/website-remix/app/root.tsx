0f20a08c20e38381f3091ce8b106540c2ed29786
Raul de Melo <melo.raulf@gmail.com>
2022-12-08 17:26:33 +0100

    feat: add base structure data and base meta

--- apps/website-remix/app/root.tsx
+++ apps/website-remix/app/root.tsx
@@ -1,7 +1,7 @@
 import prismStyles from '@raulmelo/ui/styles/prism.css';
 import baseUiStyles from '@raulmelo/ui/styles/style.css';
 import appStyles from './styles/app.css';
-import type { LoaderArgs, MetaFunction } from '@remix-run/node';
+import type { LoaderArgs, MetaFunction, SerializeFrom } from '@remix-run/node';
 import { json } from '@remix-run/node';
 import {
   Links,
@@ -12,14 +12,72 @@ import {
   ScrollRestoration,
   useLoaderData,
 } from '@remix-run/react';
+import type { PublicEnv } from '$infrastructure/config/publicAppConfig';
 import { getPublicEnvironmentVariables } from '$infrastructure/config/publicAppConfig';
+import type { StructuredDataFunction } from 'remix-utils';
 import { StructuredData } from 'remix-utils';
+import type { ISiteData } from '@raulmelo/core/dist/types/domains/siteData';
+import { domains } from '@raulmelo/core';
+import { getSEOTags } from '$infrastructure/utils/seo';
+import type { Organization, Person } from 'schema-dts';
 
-export const meta: MetaFunction = () => ({
-  charset: `utf-8`,
-  title: `New Remix App`,
-  viewport: `width=device-width,initial-scale=1`,
-});
+type LoaderData = {
+  ENV: PublicEnv;
+  locale: string;
+  siteData: ISiteData;
+  pathname: string;
+  origin: string;
+  url: string;
+};
+
+const structuredData: StructuredDataFunction<
+  SerializeFrom<LoaderData>,
+  any
+> = ({ data }) => {
+  const { siteData, url, origin } = data;
+
+  return [
+    {
+      '@type': `Person`,
+      name: siteData.personalInformation.fullName,
+      url: siteData.site.url,
+      sameAs: siteData.socials.map((social) => social.url),
+    } as Person,
+    {
+      '@type': `Organization`,
+      logo: `${origin}/logo.png`,
+      url,
+    } as Organization,
+  ];
+};
+
+export const handle = { structuredData };
+
+export const meta: MetaFunction = ({ data }) => {
+  const { siteData, locale, pathname } = data as LoaderData;
+
+  const defaultSeo = siteData.defaultSeo[locale];
+
+  return {
+    charset: `utf-8`,
+    viewport: `width=device-width,initial-scale=1`,
+    ...getSEOTags({
+      type: `website`,
+      url: `${siteData.site.url}${pathname}`,
+      title: siteData.personalInformation.fullName,
+      description: defaultSeo.description,
+      image: {
+        url: `${siteData.site.seoImage.url}?w=1024&h=512&fit=crop&auto=format`,
+        width: 1024,
+        height: 512,
+        alt: siteData.personalInformation.fullName,
+      },
+    }),
+    title: `Raul Melo · ${defaultSeo.title}`,
+    monetization: `$ilp.uphold.com/Aa8j4MXjnPHg`,
+    'theme-color': `#fff`,
+  };
+};
 
 export function links() {
   return [
@@ -47,14 +105,49 @@ export function links() {
       rel: `stylesheet`,
       href: `https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;600&display=swap`,
     },
+    /**
+     * TODO: add site.webmanifest logic
+     */
+    { rel: `manifest`, href: `/site.webmanifest` },
+    {
+      rel: `mask-icon`,
+      href: `/safari-pinned-tab.svg`,
+      color: `#000000`,
+    },
+    {
+      rel: `apple-touch-icon`,
+      sizes: `180x180`,
+      href: `/apple-touch-icon.png`,
+    },
+    {
+      rel: `icon`,
+      type: `image/png`,
+      sizes: `32x32`,
+      href: `/favicon-32x32.png`,
+    },
+    {
+      rel: `icon`,
+      type: `image/png`,
+      sizes: `16x16`,
+      href: `/favicon-16x16.png`,
+    },
   ];
 }
 
-export function loader({ params }: LoaderArgs) {
+export async function loader({ params, request }: LoaderArgs) {
   const locale = params.locale || `en`;
-  return json({
+
+  const siteData = await domains.siteData.querySiteData();
+
+  const url = new URL(request.url);
+
+  return json<LoaderData>({
     ENV: getPublicEnvironmentVariables(),
     locale,
+    siteData,
+    pathname: url.pathname,
+    origin: url.origin,
+    url: url.href,
   });
 }
 
@@ -64,7 +157,6 @@ export default function App() {
   return (
     <html lang={locale}>
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <Meta />
         <Links />
         <StructuredData />
