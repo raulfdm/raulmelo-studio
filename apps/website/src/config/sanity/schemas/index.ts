d601f2b4be0c550ef4b53db86a984fdf91c98fc7
Raul Melo <melo.raulf@gmail.com>
2022-12-11 09:21:02 +0100

    refactor: decommission cms for website (#2063)

--- apps/website/src/config/sanity/schemas/index.ts
+++
@@ -1,9 +0,0 @@
-import { fitnessSchemaFields } from '@raulmelo/sanity-fitness';
-
-import { blogTypes } from './blog';
-import { customFields } from './customFields';
-
-export const schema = {
-  name: 'default',
-  types: [...blogTypes, ...fitnessSchemaFields, ...customFields],
-};
