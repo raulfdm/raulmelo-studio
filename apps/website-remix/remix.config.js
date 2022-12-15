79f8f6b17cd219de2bd085cfd468a327fda12ee8
Raul de Melo <melo.raulf@gmail.com>
2022-12-15 10:31:42 +0100

    fix: ESM dependencies on remix

--- apps/website-remix/remix.config.js
+++ apps/website-remix/remix.config.js
@@ -1,10 +1,10 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  serverBuildTarget: 'vercel',
+  serverBuildTarget: `vercel`,
   // When running locally in development mode, we use the built in remix
   // server. This does not understand the vercel lambda module format,
   // so we default back to the standard build output.
-  server: process.env.NODE_ENV === 'development' ? undefined : './server.js',
-  ignoredRouteFiles: ['**/.*'],
-  serverDependenciesToBundle: [],
+  server: process.env.NODE_ENV === `development` ? undefined : `./server.js`,
+  ignoredRouteFiles: [`**/.*`],
+  serverDependenciesToBundle: [`query-string`, `filter-obj`],
 };
