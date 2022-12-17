2ab5dded853ca6091a4f2777fc4cbeede95487f5
Raul de Melo <melo.raulf@gmail.com>
2022-12-17 20:04:42 +0100

    fix: dev script

--- packages/core/scripts/dev.sh
+++ packages/core/scripts/dev.sh
@@ -2,8 +2,7 @@
 
 pnpm concurrently \
   "pnpm exec tsc --watch --preserveWatchOutput" \
-  "LIB=scripts pnpm vite build --watch" \
-  "LIB=core pnpm vite build --watch" \
+  "pnpm vite build --watch" \
   --kill-others-on-fail \
   --restartTries=3 \
   --restartDelay=1000
