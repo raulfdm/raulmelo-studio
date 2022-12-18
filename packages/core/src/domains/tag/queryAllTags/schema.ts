d7939366b4c971464d1ea96d0c17706d3d4501a9
Raul de Melo <melo.raulf@gmail.com>
2022-12-18 09:03:51 +0100

    fix: tag schema usage

--- packages/core/src/domains/tag/queryAllTags/schema.ts
+++ packages/core/src/domains/tag/queryAllTags/schema.ts
@@ -1,9 +1,8 @@
 import { z } from 'zod';
 
-export const tagsSchema = z.array(
-  z.object({
-    slug: z.string(),
-  }),
-);
+export const tagSchema = z.object({
+  slug: z.string(),
+});
+export type Tag = z.infer<typeof tagSchema>;
 
-export type Tag = z.infer<typeof tagsSchema>;
+export const tagsSchema = z.array(tagSchema);
