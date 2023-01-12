d5b75f3d1ac9e8d8d8d6174dcb847d2d04b17fe2
Raul de Melo <melo.raulf@gmail.com>
2023-01-12 19:34:49 +0100

    refactor: remove fitness from regular CMS

--- apps/website-remix/app/infrastructure/config/sanity/schemas/index.ts
+++ apps/website-remix/app/infrastructure/config/sanity/schemas/index.ts
@@ -1,9 +1,8 @@
-import { fitnessSchemaFields } from '@raulmelo/sanity-fitness';
-
+import { youtubeVideoField } from '@raulmelo/sanity-core';
 import { blogTypes } from './blog';
 import { customFields } from './customFields';
 
 export const schema = {
   name: `default`,
-  types: [...blogTypes, ...fitnessSchemaFields, ...customFields],
+  types: [...blogTypes, ...customFields, youtubeVideoField],
 };
