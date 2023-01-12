019b11ef435a5e622c2974790305595c6c8bb61a
Raul de Melo <melo.raulf@gmail.com>
2023-01-12 18:35:58 +0100

    refactor: group routes into a folder group

--- apps/fitness-buddy/src/routes/admin/[...routes]/+page.svelte
+++ apps/fitness-buddy/src/routes/admin/[...routes]/+page.svelte
@@ -1,32 +1,30 @@
 <script lang="ts">
-  // import { Studio, defineConfig, renderStudio } from 'sanity';
-  import type { Config } from 'sanity';
-  // import { deskTool } from 'sanity/desk';
-  import { onMount } from 'svelte';
-  import { sanityBaseConfig } from '$lib/config/sanity/config';
+	import { Studio, defineConfig, renderStudio } from 'sanity';
+	import type { Config } from 'sanity';
+	// import { deskTool } from 'sanity/desk';
+	import { onMount } from 'svelte';
+	import { sanityBaseConfig } from '$lib/config/sanity/config';
 
-  // const config = defineConfig(sanityBaseConfig);
+	const config = defineConfig(sanityBaseConfig);
 
-  onMount(() => {
-    // renderStudio(document.getElementById('sanity-studio'), config);
-  });
+	onMount(() => {
+		renderStudio(document.getElementById('sanity-studio'), config);
+	});
 </script>
-
-<h1>Test</h1>
 
 <div id="sanity-studio" />
 
 <style>
-  #sanity-studio {
-    height: 100vh;
-    max-height: 100dvh;
-    overscroll-behavior: none;
-    -webkit-font-smoothing: antialiased;
-    overflow: auto;
-  }
+	#sanity-studio {
+		height: 100vh;
+		max-height: 100dvh;
+		overscroll-behavior: none;
+		-webkit-font-smoothing: antialiased;
+		overflow: auto;
+	}
 
-  /* Fix if you're using Tailwind.css */
-  *:not([data-ui='Popover__arrow']):not([data-ui='Tooltip__arrow']) > svg {
-    display: inline;
-  }
+	/* Fix if you're using Tailwind.css */
+	*:not([data-ui='Popover__arrow']):not([data-ui='Tooltip__arrow']) > svg {
+		display: inline;
+	}
 </style>
