8f51b64e3462230ef6f285e7f112619a00bec15f
Raul de Melo <melo.raulf@gmail.com>
2023-01-21 19:36:52 +0100

    remove rewrites

--- apps/website-astro/middleware.ts
+++ apps/website-astro/middleware.ts
@@ -1,9 +1,27 @@
 import { match } from '@formatjs/intl-localematcher';
-import { rewrite } from '@vercel/edge';
 import Negotiator from 'negotiator';
 
 const supportedLocales = ['en', 'pt'];
 const defaultLocale = 'en';
+
+export default function middleware(request: Request) {
+  const url = new URL(request.url);
+
+  const pathnameIsMissingLocale = supportedLocales.every(
+    (locale) =>
+      !url.pathname.startsWith(`/${locale}/`) && url.pathname !== `/${locale}`,
+  );
+
+  if (pathnameIsMissingLocale) {
+    const locale = getLocale(request);
+
+    const normalizedPathname = normalizePathname(`/${locale}/${url.pathname}`);
+
+    const nextUrl = new URL(normalizedPathname, request.url).toString();
+
+    return Response.redirect(nextUrl);
+  }
+}
 
 function getLocale(request: Request) {
   const languages = new Negotiator({
@@ -11,56 +29,6 @@ function getLocale(request: Request) {
   }).languages(supportedLocales);
 
   return match(languages, supportedLocales, defaultLocale);
-}
-
-export default function middleware(request: Request) {
-  const url = new URL(request.url);
-
-  console.log('URL REQUESTED', url.toString());
-
-  const pathnameIsMissingLocale = supportedLocales.every(
-    (locale) =>
-      !url.pathname.startsWith(`/${locale}/`) && url.pathname !== `/${locale}`,
-  );
-
-  /**
-   * Cases:
-   * - <url>/en
-   * - <url>/en/
-   *
-   * Should be actually `<url>/` and rewrite under the hood to `<url>/en`
-   */
-  if (url.pathname === '/en' || url.pathname === '/en/') {
-    console.log('/EN CASE. Redirecting', url);
-    return Response.redirect(new URL(`/`, request.url).toString());
-  }
-
-  if (pathnameIsMissingLocale) {
-    const locale = getLocale(request);
-
-    /**
-     * Since english is the default, we always rewrite instead of redirecting
-     */
-    if (locale === 'en') {
-      const nextUrl = new URL(
-        normalizePathname(`/en/${url.pathname}`),
-        request.url,
-      );
-      console.log('ENGLISH CASE. Rewriting', nextUrl.toString());
-
-      return rewrite(nextUrl);
-    }
-
-    /**
-     * If it's portuguese, then we need to redirect
-     */
-    const nextUrl = new URL(
-      normalizePathname(`/${locale}/${url.pathname}`),
-      request.url,
-    ).toString();
-    console.log('PORTUGUESE CASE. Redirecting', nextUrl);
-    return Response.redirect(nextUrl);
-  }
 }
 
 function normalizePathname(pathname: string) {
