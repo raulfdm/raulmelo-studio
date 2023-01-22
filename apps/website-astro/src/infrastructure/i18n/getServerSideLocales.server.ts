c3d8257aede48851e581ec32cd4fc60a5dfe2cfe
Raul de Melo <melo.raulf@gmail.com>
2023-01-22 08:00:41 +0100

    add localization

---
+++ apps/website-astro/src/infrastructure/i18n/getServerSideLocales.server.ts
@@ -0,0 +1,38 @@
+import type { IntlShape } from '@formatjs/intl';
+import { createIntl, createIntlCache } from '@formatjs/intl';
+import type { SupportedLanguages } from '@raulmelo/core';
+import flat from 'flat';
+
+import enLocales from './locales/en.json';
+import ptLocales from './locales/pt.json';
+
+/**
+ * Caches prevent memory leaks and improve performance by caching
+ */
+const enCache = createIntlCache();
+const ptCache = createIntlCache();
+
+const serverIntl: Record<SupportedLanguages, IntlShape<SupportedLanguages>> = {
+  en: createIntl(
+    {
+      locale: `en`,
+      messages: flat(enLocales),
+    },
+    enCache,
+  ),
+  pt: createIntl(
+    {
+      locale: `pt`,
+      messages: flat(ptLocales),
+    },
+    ptCache,
+  ),
+};
+
+export function getIntl(
+  locale: SupportedLanguages,
+): IntlShape<SupportedLanguages> {
+  return serverIntl[locale];
+}
+
+export type Intl = ReturnType<typeof getIntl>;
