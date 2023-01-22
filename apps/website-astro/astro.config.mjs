8665bd93b494fddc731feab7a6ae7858b7e14672
Raul de Melo <melo.raulf@gmail.com>
2023-01-22 08:47:39 +0100

    add solidjs

--- apps/website-astro/astro.config.mjs
+++ apps/website-astro/astro.config.mjs
@@ -3,16 +3,19 @@ import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
+import solidJs from "@astrojs/solid-js";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), solidJs()],
   output: 'server',
   adapter: vercel({
-    includeFiles: ['middleware.ts'],
+    includeFiles: ['middleware.ts']
   }),
   vite: {
     ssr: {
       external: ['@raulmelo/core'],
-      noExternal: ['@raulmelo/ui', '@raulmelo/styles'],
-    },
-  },
+      noExternal: ['@raulmelo/ui', '@raulmelo/styles']
+    }
+  }
 });
