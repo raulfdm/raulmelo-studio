2f14d749c290caf7c30cc41fc5a419a703c5c412
Raul de Melo <melo.raulf@gmail.com>
2023-02-18 17:11:51 +0100

    add share content and migrate series section to astro

--- apps/website-astro/src/ui/PortableTextPost/PortableTextPost.tsx
+++ apps/website-astro/src/ui/PortableTextPost/PortableTextPost.tsx
@@ -6,8 +6,6 @@
 // import { ArticleJsonLd, NextSeo } from 'next-seo';
 // import { useMemo } from 'react';
 
-import type { domains } from '@raulmelo/core';
-
 // import { ShareContent } from '~/components/ShareContent';
 // import { Tag, Tags } from '~/components/Tags';
 // import { useLocalization } from '~/hooks/useLocalization';
@@ -18,32 +16,7 @@ import type { domains } from '@raulmelo/core';
 // import { portableComponents } from './components/portableComponents';
 // import { PreviewBanner } from './components/PreviewBanner/PreviewBanner';
 
-type Post = Awaited<ReturnType<typeof domains.posts.queryPostBySlug>>;
-
-type PortableTextPostProps = {
-  post: Post;
-  // content: any;
-  // title: string;
-  // subtitle?: string;
-  // publishedAt: string;
-  // estimatedReadingTime: number;
-  // nextSeo?: NextSeoProps;
-  // preview?: boolean;
-  // unsplash?: {
-  //   authorName: string;
-  //   url: string;
-  // };
-  // description?: string;
-  // seriesSection?: {
-  //   top: JSX.Element | null;
-  //   bottom: JSX.Element | null;
-  // };
-  // tags?: {
-  //   _id: string;
-  //   name: string;
-  //   slug: string;
-  // }[];
-};
+type PortableTextPostProps = {};
 
 export const PortableTextPost = ({ post }: PortableTextPostProps) => {
   // const { formatDate } = useLocalization();
@@ -59,34 +32,16 @@ export const PortableTextPost = ({ post }: PortableTextPostProps) => {
   //         />
   //       </ProseContainer>
   //       {seriesSection?.bottom}
-  //       <hr className="mt-10 mb-6" />
-  //       <footer className="flex flex-wrap justify-between">
+
   //         {tags ? (
-  //           <div className="mb-4 mr-4">
-  //             <span className="block mb-4 font-extrabold text-md md:text-lg lg:text-xl md:mb-6">
-  //               Tags
-  //             </span>
   //             <Tags>
   //               {tags.map((tag) => (
-  //                 <Tag key={tag._id} className="text-base lg:text-lg">
-  //                   <Link
-  //                     href={getTagUrl(tag.slug)}
-  //                     className="underline text-secondary"
-  //                   >
-  //                     #{tag.name}
-  //                   </Link>
-  //                 </Tag>
+
   //               ))}
   //             </Tags>
   //           </div>
   //         ) : null}
-  //         {description ? (
-  //           <ShareContent
-  //             twitter={{ text: description }}
-  //             linkedIn={{ title, summary: description }}
-  //           />
-  //         ) : null}
-  //       </footer>
+
   //     </section>
   //   </>
   // );
