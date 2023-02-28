1d7adac809af7d312106e8aafe1b35eba479ebca
Raul de Melo <melo.raulf@gmail.com>
2023-02-28 15:48:59 +0100

    enhance desk structure

--- apps/website-astro/src/infrastructure/sanity/deskStructure.ts
+++ apps/website-astro/src/infrastructure/sanity/deskStructure.ts
@@ -1,47 +1,32 @@
 import type { StructureBuilder } from 'sanity/desk';
 
 export function deskStructure(S: StructureBuilder) {
+  const typeIdsToIgnore = [`siteSettings`, `personalInfo`, `media.tag`];
+
   return S.list()
-    .title(`CMS`)
-    .items([getBlogMenu(S)]);
-}
-
-function getBlogMenu(S: StructureBuilder) {
-  const singleTypesIds = [`siteSettings`, `personalInfo`];
-
-  return S.listItem()
-    .id(`blog`)
     .title(`Blog`)
-    .child(
-      S.list()
-        .title(`Blog`)
-        .items([
-          S.listItem()
-            .title(`Personal details`)
-            .child(
-              S.document()
-                .schemaType(`personalInfo`)
-                .documentId(`personalInfo`),
-            ),
-          S.listItem()
-            .title(`Website settings`)
-            .child(
-              S.document()
-                .schemaType(`siteSettings`)
-                .documentId(`siteSettings`),
-            ),
-          S.listItem()
+    .items([
+      S.listItem()
+        .title(`Personal details`)
+        .child(
+          S.document().schemaType(`personalInfo`).documentId(`personalInfo`),
+        ),
+      S.listItem()
+        .title(`Website settings`)
+        .child(
+          S.document().schemaType(`siteSettings`).documentId(`siteSettings`),
+        ),
+      S.listItem()
+        .title(`Collections`)
+        .child(
+          S.list()
             .title(`Collections`)
-            .child(
-              S.list()
-                .title(`Collections`)
-                .items(
-                  S.documentTypeListItems().filter(
-                    (listItem) =>
-                      !singleTypesIds.includes(listItem.getId() as string),
-                  ),
-                ),
+            .items(
+              S.documentTypeListItems().filter((listItem) => {
+                console.log(listItem);
+                return !typeIdsToIgnore.includes(listItem.getId() as string);
+              }),
             ),
-        ]),
-    );
+        ),
+    ]);
 }
