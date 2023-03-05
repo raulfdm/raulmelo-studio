3d1b1bee0fd218bd57a691a07744725b32e6a031
Raul de Melo <melo.raulf@gmail.com>
2023-03-05 08:32:34 +0100

    convert tags to svelte

---
+++ apps/website-astro/src/ui/Tags/Tag.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import classNames from 'classnames';
+
+  export let className: string;
+</script>
+
+<li
+  {...$$props}
+  class={classNames(
+    'flex flex-col items-center justify-center mr-4',
+    className,
+  )}
+>
+  <slot />
+</li>
