5007d5743ad7758e6ab9d6f1d3c0694291895a9a
Raul de Melo <melo.raulf@gmail.com>
2023-03-05 12:09:58 +0100

    console VERCEL_URL

--- apps/website-astro/astro.config.mjs
+++ apps/website-astro/astro.config.mjs
@@ -2,39 +2,52 @@ import image from '@astrojs/image';
 import prefetch from '@astrojs/prefetch';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
+import svelte from '@astrojs/svelte';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless';
 import { defineConfig } from 'astro/config';
 import robotsTxt from 'astro-robots-txt';
-import svelte from "@astrojs/svelte";
+
+console.log(process.env.VERCEL_URL);
 
 // https://astro.build/config
 export default defineConfig({
   site: `https://raulmelo.dev`,
-  integrations: [tailwind(), react(), image({
-    serviceEntryPoint: `@astrojs/image/sharp`
-  }), sitemap({
-    filter: page => !page.url.includes(`/404`) && !page.url.includes(`/search`),
-    i18n: {
-      defaultLocale: `en`,
-      locales: {
-        en: `en-UK`,
-        pt: `pt-BR`
-      }
-    }
-  }), robotsTxt({
-    policy: [{
-      userAgent: `*`,
-      allow: `/`,
-      disallow: [`/search`, `/404`]
-    }]
-  }), prefetch(), svelte()],
+  integrations: [
+    tailwind(),
+    react(),
+    image({
+      serviceEntryPoint: `@astrojs/image/sharp`,
+    }),
+    sitemap({
+      filter: (page) =>
+        !page.url.includes(`/404`) && !page.url.includes(`/search`),
+      i18n: {
+        defaultLocale: `en`,
+        locales: {
+          en: `en-UK`,
+          pt: `pt-BR`,
+        },
+      },
+    }),
+    robotsTxt({
+      policy: [
+        {
+          userAgent: `*`,
+          allow: `/`,
+          disallow: [`/search`, `/404`],
+        },
+      ],
+    }),
+    prefetch(),
+    svelte(),
+  ],
   output: `server`,
   adapter: vercel({}),
   vite: {
     ssr: {
       external: [`@raulmelo/core`, `@raulmelo/ui`, `@formatjs/intl`],
-      noExternal: [`@raulmelo/styles`]
-    }
-  }
+      noExternal: [`@raulmelo/styles`],
+    },
+  },
 });
