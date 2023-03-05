87e2850dbb7ab1651a89334f16eb0e6cd19c84e0
Raul de Melo <melo.raulf@gmail.com>
2023-03-05 19:35:05 +0100

    fix wrong class

--- apps/website-astro/src/ui/SideMenu/SideMenu.svelte
+++ apps/website-astro/src/ui/SideMenu/SideMenu.svelte
@@ -22,7 +22,7 @@
 
 <Disclosure
   class={classNames([
-    'absolute top-0 bottom-0 left-0 right-0 z-20',
+    'absolute top-0 bottom-0 left-0 right-0',
     $sideMenuStore ? 'z-20' : '-z-20',
   ])}
 >
