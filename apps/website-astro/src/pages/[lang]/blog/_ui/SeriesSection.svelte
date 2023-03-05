9cb192920a108d6c0e91581a40f00a90959d5284
Raul de Melo <melo.raulf@gmail.com>
2023-03-05 12:04:37 +0100

    migrate series section to svelte

---
+++ apps/website-astro/src/pages/[lang]/blog/_ui/SeriesSection.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+  import classNames from 'classnames';
+  import { type PostBySlug } from './types';
+
+  import { IconChevronDown } from '@tabler/icons-svelte';
+
+  type Series = NonNullable<PostBySlug[`series`]>;
+
+  export let currentPostId: string;
+  export let series: Series;
+  export let seriesLabel: string;
+</script>
+
+<section>
+  <div
+    class="relative my-8 duration-200 bg-white rounded shadow dark:bg-blue-800 transition-theme ease"
+    data-testid="series-menu"
+  >
+    <div>
+      <div
+        class={classNames([
+          `flex content-between cursor-pointer px-4 py-3`,
+          `text-lg font-bold md:text-xl duration-300 transition-spacing`,
+          `pb-2.5 border-b border-gray-100 dark:border-gray-600`,
+        ])}
+        data-testid="expand-button"
+        role="button"
+      >
+        <span class="flex-1">{series.name}</span>
+        <button class="flex items-center justify-center w-7 h-7">
+          <IconChevronDown className="w-5" />
+        </button>
+      </div>
+
+      <ul class="pb-0 m-0" data-testid="series-post-list">
+        {#each series.posts as { _id, seriesCopy, slug }}
+          <li
+            class={classNames(
+              `cursor-pointer m-0 font-sans text-sm md:text-base`,
+              {
+                'bg-green-400 hover:bg-green-400 hover:bg-opacity-50':
+                  _id === currentPostId,
+              },
+            )}
+            data-testid={`post_${_id}`}
+          >
+            <a href={slug} class="block px-4 py-3 no-underline">
+              {seriesCopy}
+            </a>
+          </li>
+        {/each}
+      </ul>
+
+      <div
+        class={classNames([
+          `flex content-between cursor-pointer px-4 py-3`,
+          `font-sans text-base md:text-md duration-300 transition-spacing`,
+          `pt-2.5 border-t border-gray-100 dark:border-gray-600`,
+        ])}
+        role="button"
+      >
+        <span>{seriesLabel}</span>
+      </div>
+    </div>
+  </div>
+</section>
