5ddb0d28c6fcdbea0629fb1738356640cb17c62a
Raul de Melo <melo.raulf@gmail.com>
2023-03-05 13:02:13 +0100

    enhance side menu experience

--- apps/website-astro/src/ui/SideMenu/SideMenu.svelte
+++ apps/website-astro/src/ui/SideMenu/SideMenu.svelte
@@ -4,6 +4,7 @@
   import { type SupportedLanguages } from '@raulmelo/core/config';
   import { Disclosure, DisclosurePanel } from '@rgossiaux/svelte-headlessui';
   import classNames from 'classnames';
+  import { fly, fade } from 'svelte/transition';
   import { useSideMenuLinks } from './links';
 
   import SideMenuList from './SideMenuList.svelte';
@@ -15,32 +16,39 @@
   const intl = getIntl(lang);
 </script>
 
-<Disclosure>
+<Disclosure class="absolute top-0 bottom-0 left-0 right-0 z-20">
   {#if $sideMenuStore === true}
-    <DisclosurePanel
-      static
-      as="nav"
-      class={classNames([
-        'absolute bottom-0 right-0 z-20',
-        'h-full min-w-full sm:min-w-min sm:w-full sm:max-w-xs',
-        'bg-white pt-16 dark:bg-blue-800',
-        'transition-theme ease',
-      ])}
+    <div
+      transition:fly={{ x: 400, opacity: 1, duration: 200 }}
+      class="relative bottom-0 right-0 z-20 h-full"
     >
-      <SideMenuList>
-        {#each links as link}
-          <SideMenuListItem
-            {...link}
-            itemLabel={intl.formatMessage({ id: link.itemLabel })}
-          />
-        {/each}
-      </SideMenuList>
-    </DisclosurePanel>
+      <DisclosurePanel
+        static
+        as="nav"
+        class={classNames([
+          'ml-auto',
+          'h-full min-w-full sm:min-w-min sm:w-full sm:max-w-xs',
+          'bg-white pt-16 dark:bg-blue-800',
+          'transition-theme ease',
+        ])}
+      >
+        <SideMenuList>
+          {#each links as link}
+            <SideMenuListItem
+              {...link}
+              itemLabel={intl.formatMessage({ id: link.itemLabel })}
+            />
+          {/each}
+        </SideMenuList>
+      </DisclosurePanel>
+    </div>
 
-    <DisclosurePanel
-      static
-      as="div"
-      class="absolute inset-0 z-10 pointer-events-none bg-gray-900/80 top-16"
-    />
+    <div transition:fade={{ duration: 200 }} class="z-10">
+      <DisclosurePanel
+        static
+        as="div"
+        class="absolute inset-0 bg-black pointer-events-none opacity-70 top-16"
+      />
+    </div>
   {/if}
 </Disclosure>
