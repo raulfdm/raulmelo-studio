a70b0c98aaecc5afce6f063cc0dcd450c208af04
Raul de Melo <melo.raulf@gmail.com>
2023-03-11 07:52:05 +0100

    fix all fonts

--- apps/website-astro/tailwind.config.cjs
+++ apps/website-astro/tailwind.config.cjs
@@ -1,9 +1,20 @@
+// eslint-disable-next-line @typescript-eslint/quotes, @typescript-eslint/no-var-requires
+const defaultPreset = require('@raulmelo/styles/lib/tailwind.config.cjs');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  presets: [require('@raulmelo/styles/lib/tailwind.config.cjs')],
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  presets: [defaultPreset],
+  content: [`./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}`],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [
+          `InterVariable`,
+          `Inter`,
+          ...defaultPreset.theme.fontFamily.sans,
+        ],
+      },
+    },
   },
   plugins: [],
 };
