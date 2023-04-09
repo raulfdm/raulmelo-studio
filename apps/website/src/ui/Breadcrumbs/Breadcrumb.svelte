d10600fedeebe3edf3af9658e31cedb235af9e72
Raul de Melo <melo.raulf@gmail.com>
2023-04-09 13:43:25 +0200

    implement breadcrumb component

---
+++ apps/website/src/ui/Breadcrumbs/Breadcrumb.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import { BreadcrumbType } from './types';
+
+  export let crumb: BreadcrumbType;
+</script>
+
+<li class="flex items-center">
+  {#if 'href' in crumb}
+    <a href={crumb.href}>{crumb.label}</a>
+  {:else}
+    {crumb.label}
+  {/if}
+</li>
+
+<style>
+  li {
+    & > a {
+      &:hover {
+        @apply underline;
+      }
+    }
+    & + *:before {
+      content: '';
+      @apply ml-2 mr-3 block h-1.5 w-1.5 rotate-45 transform opacity-40;
+      border-top: 1px solid;
+      border-right: 1px solid;
+      background-color: transparent;
+    }
+  }
+</style>
---
+++ apps/website/src/ui/Breadcrumbs/Breadcrumbs.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import classNames from 'classnames';
+  import Breadcrumb from './Breadcrumb.svelte';
+  import { type BreadcrumbType } from './types';
+
+  export let crumbs: BreadcrumbType[];
+  let className = '';
+
+  export { className as class };
+</script>
+
+<div class={classNames('py-2', className)}>
+  <ul class="flex items-center whitespace-nowrap min-h-min">
+    {#each crumbs as crumb}
+      <Breadcrumb {crumb} />
+    {/each}
+  </ul>
+</div>
