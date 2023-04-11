2e9c2c3f24de0c52afa2b30395a24abd83d597c0
Raul de Melo <melo.raulf@gmail.com>
2023-04-11 11:47:52 +0200

    add script to migrate measurements

--- misc/playground/src/cms/migrateMeasurements.js
+++ misc/playground/src/cms/migrateMeasurements.js
@@ -3,7 +3,6 @@ import * as url from 'node:url';
 
 import { createClient } from '@sanity/client';
 import * as dotenv from 'dotenv';
-import got from 'got';
 import groq from 'groq';
 
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
@@ -32,59 +31,33 @@ const newClient = createClient({
 });
 
 const query = groq`
-*[_type == "exercise"]{
+*[_type == "measurementSchema"]{
   ...,
-  "image": image.asset-> {
-    url
-  }
 }
 `;
+
 let counter = 0;
 const fails = [];
 
 try {
-  const exercises = await raulClient.fetch(query);
+  const measurements = await raulClient.fetch(query);
 
-  console.log('Number of exercises:', exercises.length);
-
-  exercises.forEach((exercise) => {
-    if (exercise.image === undefined) {
-      console.log(exercise._id);
-    }
-  });
+  console.log('Number of routines:', measurements.length);
 
   const transactions = newClient.transaction();
 
   console.log('Start the migration');
-  for await (const exercise of exercises) {
+  for await (const measurement of measurements) {
     console.group();
-    const { _id, _type, name, video, image } = exercise;
 
-    console.log(`${++counter}. ${name}`);
+    console.log(`Routine ${++counter}`);
 
     try {
-      const imageStream = got.stream(image.url);
-      console.log('Uploading image');
-      const sanityImage = await newClient.assets.upload('image', imageStream);
-      console.log('Image uploaded');
-
-      transactions.createOrReplace({
-        _id,
-        _type,
-        name,
-        video,
-        image: {
-          _type: 'image',
-          asset: {
-            _ref: sanityImage._id,
-            _type: 'reference',
-          },
-        },
-      });
+      transactions.createOrReplace(measurement);
       console.log('Item added\n--------');
     } catch (error) {
       console.log('FAIL');
-      fails.push({ _id, error, exercise });
+      fails.push({ id: measurement._id, error, measurement });
     }
 
     console.groupEnd();
