9a193f8c1b1ec56f4ac6aff40d42f3531d5c179f
Raul de Melo <melo.raulf@gmail.com>
2023-04-16 14:30:42 +0200

    replace all usage

--- apps/website/src/env.d.ts
+++ apps/website/src/env.d.ts
@@ -8,21 +8,3 @@ declare global {
     __theme: `light` | `dark`;
   }
 }
-
-interface ImportMetaEnv {
-  /**
-   * Server only
-   */
-  APP_ENV: `local` | `preview` | `production`;
-  ALGOLIA_ADMIN_KEY: string;
-  ADMIN_PASSWORD: string;
-  /**
-   * Client and Server
-   */
-  PUBLIC_ALGOLIA_APP_ID: string;
-  PUBLIC_ALGOLIA_SEARCH_KEY: string;
-  PUBLIC_ALGOLIA_INDEX_NAME: string;
-  PUBLIC_GOOGLE_ANALYTICS_ID: string;
-}
-
-export {};
--- apps/website/src/pages/[lang]/search/_ui/searchClient.ts
+++ apps/website/src/pages/[lang]/search/_ui/searchClient.ts
@@ -1,16 +1,7 @@
+import { clientEnv } from '@/infrastructure/env/client';
 import algoliaSearch from 'algoliasearch/lite';
-import invariant from 'tiny-invariant';
-
-invariant(
-  import.meta.env.PUBLIC_ALGOLIA_APP_ID,
-  `Missing PUBLIC_ALGOLIA_APP_ID`,
-);
-invariant(
-  import.meta.env.PUBLIC_ALGOLIA_SEARCH_KEY,
-  `Missing PUBLIC_ALGOLIA_SEARCH_KEY`,
-);
 
 export const searchClient = algoliaSearch(
-  import.meta.env.PUBLIC_ALGOLIA_APP_ID,
-  import.meta.env.PUBLIC_ALGOLIA_SEARCH_KEY,
+  clientEnv.PUBLIC_ALGOLIA_APP_ID,
+  clientEnv.PUBLIC_ALGOLIA_SEARCH_KEY,
 );
