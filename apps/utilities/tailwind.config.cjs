58c3e48cc8f6ed8c53af1c0a880089eee10d2b46
Raul Melo <melo.raulf@gmail.com>
2023-04-21 20:15:37 +0200

    add skeleton

--- apps/utilities/tailwind.config.cjs
+++ apps/utilities/tailwind.config.cjs
@@ -1,5 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  darkMode: 'class',
+  content: [
+    './src/**/*.{html,js,svelte,ts}',
+    require('path').join(
+      require.resolve('@skeletonlabs/skeleton'),
+      '../**/*.{html,js,svelte,ts}',
+    ),
+  ],
   theme: {},
-  plugins: [],
+  plugins: [...require('@skeletonlabs/skeleton/tailwind/skeleton.cjs')()],
 };
