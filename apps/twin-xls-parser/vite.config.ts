1d5035816a95e6fd542761f50c547245b2fe17de
Raul Melo <melo.raulf@gmail.com>
2023-05-02 12:52:15 +0200

    change port

--- apps/twin-xls-parser/vite.config.ts
+++ apps/twin-xls-parser/vite.config.ts
@@ -3,6 +3,10 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	server: {
+		port: 5174,
+		strictPort: false
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}'],
 		globals: true,
