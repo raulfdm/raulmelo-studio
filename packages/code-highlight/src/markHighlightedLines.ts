ab1bae7f1b70c030c35f18be818b8ed283a26586
Raul Melo <melo.raulf@gmail.com>
2023-05-08 21:05:06 +0200

    fix code line highlight

--- packages/code-highlight/src/markHighlightedLines.ts
+++ packages/code-highlight/src/markHighlightedLines.ts
@@ -7,8 +7,20 @@ export function markHighlightedLines(tree: Root, highlightedLines: string) {
     return tree;
   }
 
+  /**
+   * For some reason, there are a bunch of `text` nodes in the tree
+   * with breaking lines. We need to filter them out to apply the line highlight
+   * properly.
+   */
+  const childrenToCheck = tree.children.filter(
+    (child) => child.type === 'element' && child.tagName === 'span',
+  );
+
   for (const lineToMark of linesToMark) {
-    const line = tree.children[lineToMark];
+    /**
+     * -1 because the array is 0-based and the lines are 1-based.
+     */
+    const line = childrenToCheck[lineToMark - 1];
 
     if (line && 'properties' in line) {
       line.properties!.dataHighlightLine = true;
