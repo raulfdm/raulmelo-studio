f75cdfa796248733742c7981a33a0484bf8b445d
Raul Melo <melo.raulf@gmail.com>
2023-05-15 09:49:50 +0200

    implement endpoint to update algolia

--- apps/website/src/infrastructure/env/server.ts
+++ apps/website/src/infrastructure/env/server.ts
@@ -3,6 +3,7 @@ import { z } from 'zod';
 const serverEnvSchema = z.object({
   APP_ENV: z.enum([`local`, `preview`, `production`]),
   SANITY_TOKEN: z.string(),
+  ALGOLIA_ADMIN_KEY: z.string(),
 });
 
 export const serverEnv = serverEnvSchema.parse(import.meta.env);
