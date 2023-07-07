f226609883c0a7321a951af2be248c0db86db668
Raul Melo <melo.raulf@gmail.com>
2023-07-07 06:57:55 +0200

    add more lint and fix all problems

--- apps/website/src/ui/AuthorPresentation/index.ts
+++ apps/website/src/ui/AuthorPresentation/index.ts
@@ -1,3 +1,3 @@
-import AuthorPresentation from './AuthorPresentation.astro';
-
-export { AuthorPresentation };
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+export { default as AuthorPresentation } from './AuthorPresentation.astro';
