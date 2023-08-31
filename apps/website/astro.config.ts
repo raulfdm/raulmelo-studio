104e4ef9f488ad5d277b6cc2807995dc18c153ff
Raul de Melo <melo.raulf@gmail.com>
2023-08-31 12:48:30 +0200

    remove million js

--- apps/website/astro.config.ts
+++ apps/website/astro.config.ts
@@ -7,7 +7,6 @@ import vercel from '@astrojs/vercel/serverless';
 import { defineConfig, sharpImageService } from 'astro/config';
 import robotsTxt from 'astro-robots-txt';
 import { resolve } from 'import-meta-resolve';
-import million from 'million/compiler';
 import { loadEnv } from 'vite';
 
 const { VERCEL_ENV, VERCEL_URL } = loadEnv(
@@ -71,12 +70,6 @@ const config = defineConfig({
     },
   }),
   vite: {
-    /**
-     * TODO: got broken after migrating to v3
-     */
-    plugins: [
-      million.vite({ mode: 'react', server: true, auto: true, mute: true }),
-    ],
     ssr: {
       external: [`@raulmelo/core`, `@raulmelo/code-highlight`],
     },
