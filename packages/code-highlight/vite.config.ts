99950e8180573b8dcff9e86c27476ff777375fdd
Raul Melo <melo.raulf@gmail.com>
2023-09-13 08:45:29 +0200

    simplify code-highlight

--- packages/code-highlight/vite.config.ts
+++ packages/code-highlight/vite.config.ts
@@ -1,18 +1,21 @@
 /// <reference types="vitest" />
 
 import path from 'node:path';
+
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 function isExternal(id: string) {
   return !id.startsWith('.') && !path.isAbsolute(id);
 }
 
 export default defineConfig({
+  plugins: [dts()],
   build: {
     target: 'node18',
     outDir: 'dist',
     sourcemap: true,
-    emptyOutDir: false,
+    emptyOutDir: process.env.NODE_ENV === 'production',
     lib: {
       entry: 'src/index.ts',
       formats: ['es'],
