2211d3b70e75b0fc05a6d4c450a8119a1f879830
Raul Melo <melo.raulf@gmail.com>
2023-09-13 07:47:14 +0200

    remove scripty from core

--- packages/core/scripts/dev.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-pnpm concurrently \
-  "pnpm exec tsc --watch --preserveWatchOutput" \
-  "pnpm vite build --watch" \
-  --kill-others-on-fail \
-  --restartTries=3 \
-  --restartDelay=1000
