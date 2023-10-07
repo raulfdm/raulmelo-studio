befa938a455fa61922d8274d8bfd88904dc3204f
Raul Melo <melo.raulf@gmail.com>
2023-10-07 14:49:38 -0300

    replace classname dependency

--- apps/website/src/infrastructure/utils/misc.ts
+++ apps/website/src/infrastructure/utils/misc.ts
@@ -1,6 +1,6 @@
-import classNames from 'classnames';
+import classNames, { type ClassArray } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
-export function mergeClasses(...classes: classNames.ArgumentArray): string {
+export function mergeClasses(...classes: ClassArray): string {
   return twMerge(classNames(...classes));
 }
