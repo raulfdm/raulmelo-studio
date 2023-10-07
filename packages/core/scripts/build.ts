396f8a9e13bd647cf0b475f69ea36a0589a0e6fa
Raul Melo <melo.raulf@gmail.com>
2023-10-07 13:15:33 -0300

    add bun scripts

---
+++ packages/core/scripts/build.ts
@@ -0,0 +1,23 @@
+import pkg from '../package.json';
+
+const result = await Bun.build({
+  entrypoints: [
+    './src/domains/index.ts',
+    './src/config/index.ts',
+    './src/scripts/index.ts',
+    './src/utils/index.ts',
+  ],
+  outdir: './dist',
+  target: 'node',
+  format: 'esm',
+  external: [...Object.keys(pkg.dependencies)],
+  splitting: true,
+});
+
+if (result.success === false) {
+  console.error(result.logs);
+  process.exit(1);
+} else {
+  console.log('Build success!');
+  process.exit(0);
+}
