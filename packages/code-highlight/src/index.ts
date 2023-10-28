6105defd6e509fc3d8af2983f7ac055293537d5f
Raul de Melo <melo.raulf@gmail.com>
2023-10-28 13:19:00 -0300

    ignore tsbuildinfo

--- packages/code-highlight/src/index.ts
+++ packages/code-highlight/src/index.ts
@@ -1,8 +1,8 @@
 import { toHtml } from 'hast-util-to-html';
 
-import { addLineNumber } from './addLineNumber.ts';
-import { starryNight } from './configuredStarry.ts';
-import { markHighlightedLines } from './markHighlightedLines.ts';
+import { addLineNumber } from './addLineNumber.js';
+import { starryNight } from './configuredStarry.js';
+import { markHighlightedLines } from './markHighlightedLines.js';
 
 type CodeHighlightOptions = {
   language?: string;
