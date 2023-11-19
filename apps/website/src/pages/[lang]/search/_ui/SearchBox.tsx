3efa4ab6f84462c68f7092b8c7d4e3fe3bcea3cf
Raul Melo <melo.raulf@gmail.com>
2023-11-19 08:57:51 -0300

    use react-instantsearch instead hooks (#2605)

--- apps/website/src/pages/[lang]/search/_ui/SearchBox.tsx
+++ apps/website/src/pages/[lang]/search/_ui/SearchBox.tsx
@@ -1,4 +1,4 @@
-import { SearchBox as SearchBoxComp } from 'react-instantsearch-hooks-web';
+import { SearchBox as SearchBoxComp } from 'react-instantsearch';
 
 export function SearchBox({ placeholder }: { placeholder: string }) {
   return (
