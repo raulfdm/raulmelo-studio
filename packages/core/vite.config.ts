7f8aedc23c5a389f60c17af22f89a658e0582336
Raul de Melo <melo.raulf@gmail.com>
2023-11-20 12:29:00 -0300

    Update build script to use Vite

---
+++ packages/core/vite.config.ts
@@ -0,0 +1,39 @@
+import * as path from 'node:path';
+import * as url from 'node:url';
+
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import tsPaths from 'vite-tsconfig-paths';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+export default defineConfig({
+  plugins: [
+    tsPaths(),
+    dts({
+      tsconfigPath: './tsconfig.build.json',
+    }),
+  ],
+  build: {
+    outDir: path.resolve(__dirname, './dist'),
+    lib: {
+      formats: ['es'],
+      entry: {
+        'src/config/index': path.resolve(__dirname, './src/config/index.ts'),
+        'src/domains/index': path.resolve(__dirname, './src/domains/index.ts'),
+        'src/scripts/index': path.resolve(__dirname, './src/scripts/index.ts'),
+        'src/utils/index': path.resolve(__dirname, './src/utils/index.ts'),
+      },
+    },
+    rollupOptions: {
+      external: isExternal,
+    },
+  },
+});
+
+function isExternal(id: string) {
+  const isAlias = id.startsWith('@/');
+  const isRamda = id.startsWith('ramda/');
+
+  return !isRamda && !isAlias && !id.startsWith('.') && !path.isAbsolute(id);
+}
