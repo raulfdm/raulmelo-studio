589883dcef41d4286cf1d9ebd003c42dfa1c23d7
Raul de Melo <melo.raulf@gmail.com>
2023-12-03 12:27:44 -0300

    make base docker image flexible

--- scripts/build-base-docker-image.sh
+++ scripts/build-base-docker-image.sh
@@ -1,3 +1,10 @@
+export BUN_VERSION=1.0.15
+export PNPM_VERSION=8.11.0
+export NODE_VERSION=18
+
 docker build . \
-  -t raulmelo-node-18-pnpm-8.11-bun-1.0.14 \
+  --build-arg BUN_VERSION=$BUN_VERSION \
+  --build-arg PNPM_VERSION=$PNPM_VERSION \
+  --build-arg NODE_VERSION=$NODE_VERSION \
+  -t raulmelo-node-$NODE_VERSION-pnpm-$PNPM_VERSION-bun-$BUN_VERSION \
   -f docker/base-image/Dockerfile
