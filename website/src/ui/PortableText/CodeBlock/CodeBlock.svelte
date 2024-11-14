b98649f2d2b825430a4e00a3b857ade70c6fbf95
Raul Melo <melo.raulf@gmail.com>
2024-11-14 07:19:26 +0100

    fix code block

--- website/src/ui/PortableText/CodeBlock/CodeBlock.svelte
+++ website/src/ui/PortableText/CodeBlock/CodeBlock.svelte
@@ -2,6 +2,7 @@
   import { useMachine } from '@xstate/svelte';
   import { copyMachine } from './copyMachine';
   import { ClipboardIcon, ClipboardCheckIcon } from 'lucide-svelte';
+  import { mergeClasses } from '@/infrastructure/utils/misc';
 
   interface Props {
     code: string;
@@ -26,16 +27,38 @@
   }
 </script>
 
-<div class="relative code-snippet">
-  <div class="actions">
+<div
+  class={mergeClasses(
+    'code-snippet',
+    'relative',
+    'border border-neutral-200 dark:border-gray-700 rounded-lg',
+    'mb-8',
+  )}
+>
+  <div
+    class={mergeClasses(
+      ' bg-neutral-100 dark:bg-gray-800',
+      'rounded-t-lg py-2 px-[var(--padding)]',
+      'justify-between items-center flex',
+      'border-b border-b-neutral-200 dark:border-b-gray-700',
+    )}
+  >
     {#if filename}
-      <span class="filename">{filename}</span>
+      <span class="text-[14px] text-gray-800 dark:text-gray-200"
+        >{filename}</span
+      >
     {/if}
 
     <button
       type="button"
       title={copyTitle}
-      class="copy-btn focus:outline-white hover:text-gray-300"
+      class={mergeClasses(
+        'focus:outline-white hover:text-gray-300',
+        'p-0.5 text-gray-500',
+        'transition-colors',
+        'rounded',
+        'ml-auto',
+      )}
       onclick={onCopyCode}
     >
       {#if $snapshot.matches('copied')}
@@ -46,40 +69,21 @@
     </button>
   </div>
 
-  <pre><code class:showLines={showLineNumbers}>{@html codeHtml}</code></pre>
+  <pre
+    class={mergeClasses(
+      'bg-neutral-100 dark:bg-gray-800',
+      'rounded-t-none',
+      'px-0 m-0',
+    )}><code
+      class:showLines={showLineNumbers}
+      class={mergeClasses(
+        'inline-grid w-full px-[var(--padding)] text-primary',
+      )}>{@html codeHtml}</code
+    ></pre>
 </div>
 
 <style lang="postcss">
   .code-snippet {
     --padding: 20px;
-
-    @apply border border-neutral-200 dark:border-gray-700 rounded-lg;
-    @apply mb-8;
-  }
-
-  .actions {
-    @apply bg-neutral-300 dark:bg-gray-800;
-    @apply rounded-t-lg py-2 px-[var(--padding)];
-    @apply justify-between items-center flex;
-    @apply border-b border-b-neutral-200 dark:border-b-gray-700;
-  }
-
-  .filename {
-    @apply text-[14px] text-gray-800 dark:text-gray-200;
-  }
-
-  .copy-btn {
-    @apply p-0.5 text-gray-500 transition-colors rounded;
-    @apply ml-auto;
-  }
-
-  pre {
-    @apply bg-neutral-100 dark:bg-gray-800;
-    @apply rounded-t-none;
-    @apply px-0 m-0;
-  }
-
-  code {
-    @apply inline-grid w-full px-[var(--padding)] text-primary;
   }
 </style>
