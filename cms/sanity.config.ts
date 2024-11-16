b1590444cd3b50143cbda960374a3003af8763d1
Raul Melo <melo.raulf@gmail.com>
2024-11-16 07:25:12 +0100

    add some plugins

--- cms/sanity.config.ts
+++ cms/sanity.config.ts
@@ -1,6 +1,8 @@
+import {documentInternationalization} from '@sanity/document-internationalization'
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
+import {media} from 'sanity-plugin-media'
 import {schema} from './schema'
 
 export default defineConfig({
@@ -10,7 +12,17 @@ export default defineConfig({
   projectId: 'gc3hakk3',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
-
-  schema,
+  plugins: [
+    structureTool(),
+    visionTool(),
+    documentInternationalization({
+      supportedLanguages: [
+        {id: 'en', title: 'English'},
+        {id: 'pt', title: 'Portuguese'},
+      ],
+      schemaTypes: ['post', 'til'],
+    }) as never,
+    media() as never,
+  ],
+  schema: schema as never,
 })
