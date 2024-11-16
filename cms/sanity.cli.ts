299bba06a8400e08f9be399bf22b731a16c371f2
Raul Melo <melo.raulf@gmail.com>
2024-11-16 07:39:17 +0100

    fixes build

--- cms/sanity.cli.ts
+++ cms/sanity.cli.ts
@@ -10,4 +10,14 @@ export default defineCliConfig({
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
    */
   autoUpdates: true,
+  vite: {
+    build: {
+      /**
+       * Fixes this:
+       *
+       * static/sanity-!~{001}~.js:21651:0: ERROR: Top-level await is not available in the configured target environment ("chrome87", "edge88", "es2020", "firefox78", "safari14" + 3 overrides)
+       */
+      target: 'es2022',
+    },
+  },
 })
