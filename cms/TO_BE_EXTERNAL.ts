b356efe9f9c4cc1b95d1c9d004047af933bc4acf
Raul Melo <melo.raulf@gmail.com>
2024-11-16 13:40:28 +0100

    remove usage

--- cms/TO_BE_EXTERNAL.ts
+++ cms/TO_BE_EXTERNAL.ts
@@ -1,5 +1,2 @@
-export const SUPPORTED_LANGUAGES = ['en', 'pt'] as const
-export type SupportedLanguages = (typeof SUPPORTED_LANGUAGES)[number]
-
 export const calloutTypes = [`note`, `info`, `warn`] as const
 export type CalloutType = (typeof calloutTypes)[number]
