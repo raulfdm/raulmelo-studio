c6e3720b6e549c733207b579a1efb980c6b19872
Raul Melo <melo.raulf@gmail.com>
2024-11-17 10:00:30 +0100

    move the secrets handler to secrets

--- packages/core/sanity.ts
+++ packages/core/sanity.ts
@@ -4,14 +4,14 @@ const sanityEnv = z.object({
   SANITY_TOKEN: z.string(),
 });
 
-export function createConfig(envVars: Record<string, string | undefined>) {
+export function createConfig() {
   return {
     projectId: 'gc3hakk3',
     dataset: 'production',
     apiVersion: 'v2021-10-21',
     perspective: 'published',
     studioHost: 'raulmelo',
-    get secrets() {
+    secrets(envVars: Record<string, string | undefined>) {
       return sanityEnv.parse(envVars);
     },
   } as const;
