ab46ff99833cc2d8dd8932707d186cf746a2f553
Raul Melo <melo.raulf@gmail.com>
2024-11-17 17:06:59 +0100

    Update dependency bits-ui to v1.0.0-next.58 (#2792)

--- scripts/bun-update-lock.ts
+++ scripts/bun-update-lock.ts
@@ -1,1 +1,28 @@
-console.log(process.env);
+import { $ } from 'bun';
+// GITHUB_REF_NAME: e.g., renovate/bits-ui-1.x
+invariant(process.env.GITHUB_REF_NAME, 'GITHUB_REF_NAME is not defined');
+
+if (!process.env.GITHUB_REF_NAME.includes('renovate')) {
+  console.log('Not a renovate branch, skipping');
+  process.exit(0);
+}
+
+await $`bun i`;
+
+await $`git config --global user.email "bun-bot@raulmelo.me"`;
+await $`git config --global user.name "Bun Bot"`;
+
+await $`git add bun.lockb`;
+
+await $`git commit -m "chore: update bun.lock"`;
+
+await $`git push origin HEAD`;
+
+function invariant<T>(
+  value: T | undefined | null,
+  message: string = 'Invariant failed',
+): asserts value is T {
+  if (value === undefined || value === null || value === false) {
+    throw new Error(message);
+  }
+}
