511bb8743ea5d7e9b4d64d7a5b9157272e91350b
Raul Melo <melo.raulf@gmail.com>
2024-11-17 17:00:40 +0100

    tweak script

--- scripts/bun-update-lock.ts
+++ scripts/bun-update-lock.ts
@@ -1,1 +1,21 @@
-console.log(process.env);
+import { $ } from 'bun';
+// GITHUB_REF_NAME: e.g., renovate/bits-ui-1.x
+invariant(process.env.GITHUB_REF_NAME, 'GITHUB_REF_NAME is not defined');
+
+if (!process.env.GITHUB_REF_NAME.includes('renovate')) {
+  console.log('Not a renovate branch, skipping');
+  process.exit(0);
+}
+
+await $`bun i`;
+
+await $`ls -la`;
+
+function invariant<T>(
+  value: T | undefined | null,
+  message: string = 'Invariant failed',
+): asserts value is T {
+  if (value === undefined || value === null || value === false) {
+    throw new Error(message);
+  }
+}
