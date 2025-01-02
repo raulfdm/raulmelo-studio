720c38dc48efea3698a96bd14f652652bbac549e
Raul Melo <melo.raulf@gmail.com>
2025-01-02 11:14:27 +0100

    add vercel adapter and set to server

--- website-new/astro.config.mjs
+++ website-new/astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
+});
