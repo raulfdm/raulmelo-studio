dfabdcd0283c56f23cb7da69af41ae0fedf4d175
Raul Melo <melo.raulf@gmail.com>
2025-01-11 19:40:49 +0100

    fix site

--- website-new/astro.config.mjs
+++ website-new/astro.config.mjs
@@ -5,11 +5,14 @@ import { i18nConfig } from './src/lib/config/locale';
 
 // https://astro.build/config
 export default defineConfig({
+  // site must be set so the locale routing can work
   get site() {
     let site;
 
+    // On Vercel, they provide a URL via this env var
+    // https://vercel.com/docs/projects/environment-variables/system-environment-variables#VERCEL_URL
     if (process.env.VERCEL_URL) {
-      site = process.env.VERCEL_URL;
+      site = `https://${process.env.VERCEL_URL}`;
     } else {
       return `http://localhost:4321`;
     }
